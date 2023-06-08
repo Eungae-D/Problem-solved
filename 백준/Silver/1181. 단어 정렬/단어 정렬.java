dda41d3f3dc961b15252e3ac80cccdaf2235198a
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2023-06-09 01:20:08 +0900

    [Silver V] Title: 단어 정렬, Time: 880 ms, Memory: 33360 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        String[] arr = new String[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.next();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(arr[0]);
+
+        for(int i = 1; i < size;i++){
+            if(!arr[i].equals(arr[i-1])){
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
