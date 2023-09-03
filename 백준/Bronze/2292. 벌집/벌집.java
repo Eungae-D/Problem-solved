c62521a0c29ef3316bceca4130d8da2230ae9fae
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2023-09-03 23:52:42 +0900

    [Bronze II] Title: 벌집, Time: 108 ms, Memory: 12860 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int count = 1;
+		int range = 2;
+
+		if (input == 1) {
+			System.out.println(1);
+		}
+
+		else {
+			while (range <= input) {
+				range = range + (6 * count);
+				count++;
+
+			}
+			System.out.println(count);
+		}
+
+	}
+
+}
