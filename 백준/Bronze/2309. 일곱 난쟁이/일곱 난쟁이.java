dbba5a8a23855c62542e96c0497d76e9a55d1b92
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-01-07 20:32:56 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 216 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,35 +1,36 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		int[] arr = new int[9];
+        int[] arr= new int[9];
+        int sum=0;
 
-		int sum = 0;
-		// 배열에 넣어
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt();
-			sum += arr[i];
-		}
+        for(int i = 0; i < arr.length; i++){
+            int input = sc.nextInt();
 
-		Arrays.sort(arr);
+            sum+=input;
+            arr[i] = input;
 
-		outer: for (int i = 0; i < 9; i++) {
-			for (int j = i + 1; j < 9; j++) {
-				if (sum - arr[i] - arr[j] == 100) {
-					for (int k = 0; k < 9; k++) {
-						if (i == k || j == k) {
-							continue;
-						}
-						System.out.println(arr[k]);
-					}
-					break outer;
-				}
-			}
-		}
+        }
 
-	}
+        Arrays.sort(arr);
+
+
+        outer: for(int i = 0 ; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(sum-arr[i]-arr[j]==100){
+                    for(int k = 0; k < arr.length; k++){
+                        if(i == k || j == k){
+                            continue;
+                        }
+                        System.out.println(arr[k]);
+                    }
+                    break outer;
+                }
+            }
+        }
+    }
 }
