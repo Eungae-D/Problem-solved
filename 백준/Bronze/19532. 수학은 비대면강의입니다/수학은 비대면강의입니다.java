6d018bed06ca6ad8b85b718332a21f8c51af7e47
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-01-08 14:16:58 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 180 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -2,16 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int f = Integer.parseInt(st.nextToken());
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int a,b,c,d,e,f;
+
+    static void input() throws Exception{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        f = Integer.parseInt(st.nextToken());
+    }
+
+    static void process(){
 
         for(int i = -999; i <=999; i++){
             for(int j = -999; j<= 999; j++){
@@ -22,5 +30,9 @@ public class Main {
         }
     }
 
+    public static void main(String[] args) throws Exception {
+       input();
+       process();
+    }
 
 }
