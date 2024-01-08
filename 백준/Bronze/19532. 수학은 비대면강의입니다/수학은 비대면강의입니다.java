973a93591a12d9d0b4d6d9161417f46e91735090
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-01-08 14:33:25 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 140 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -3,13 +3,26 @@ import java.util.*;
 
 public class Main {
 
+    static StringBuilder sb;
     static BufferedReader br;
     static StringTokenizer st;
+    static String blank = " ";
+    static String lineBreak = "\n";
+
     static int a,b,c,d,e,f;
+
+    static void sb(){
+        sb = new StringBuilder();
+    }
+
+    static void stk() throws Exception{
+        st = new StringTokenizer(br.readLine());
+    }
 
     static void input() throws Exception{
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
+        sb();
+        stk();
 
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
@@ -20,11 +33,12 @@ public class Main {
     }
 
     static void process(){
-
         for(int i = -999; i <=999; i++){
             for(int j = -999; j<= 999; j++){
                 if(a*i+b*j==c&&d*i+e*j==f){
-                    System.out.println(i+" "+j);
+                    sb.append(i).append(blank).append(j);
+                    System.out.println(sb.toString());
+                    return;
                 }
             }
         }
