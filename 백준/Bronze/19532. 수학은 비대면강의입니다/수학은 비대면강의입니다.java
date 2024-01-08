838ccdb81626ac54f79f62bae669fe7e4dc38e84
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-01-08 14:09:31 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 208 ms, Memory: 18420 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,15 +1,17 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        int e = sc.nextInt();
-        int f = sc.nextInt();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
 
         for(int i = -999; i <=999; i++){
             for(int j = -999; j<= 999; j++){
