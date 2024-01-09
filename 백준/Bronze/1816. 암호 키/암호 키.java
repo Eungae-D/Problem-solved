2713e3569f521b6ba79e4ad07cabec4af55049ef
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-01-10 00:29:23 +0900

    [Bronze I] Title: 암호 키, Time: 316 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.java
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -1,49 +1,25 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean[] flag = new boolean[1000001];
-        ArrayList<Integer> arr = new ArrayList<>();
-
-
-        for (int i = 2; i < 1000001; i++)
-            flag[i] = true;
-
-
-        for (int i = 2; i*i < 1000001; i++) {
-            if (flag[i]) {
-                for (int j = i * i; j < 1000001; j += i)
-                    flag[j] = false;
-            }
-        }
-
-
-        for (int i = 2; i < 1000001; i++) {
-            if (flag[i])
-                arr.add(i);
-        }
-
-
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
+
+        for(int i = 0 ; i < n ; i ++){
             long m = sc.nextLong();
             boolean isPrime = true;
-
-            for (int j = 0 ; j <arr.size(); j++) {
-                if (m % arr.get(j) == 0) {
-                    System.out.println("NO");
+            for(int j = 2 ; j < 1000001; j++){
+                if(m%j == 0 ){
                     isPrime = false;
                     break;
                 }
             }
-
-            if (isPrime)
+            if(!isPrime){
+                System.out.println("NO");
+            }else{
                 System.out.println("YES");
+            }
         }
-
-        sc.close();
     }
 }
