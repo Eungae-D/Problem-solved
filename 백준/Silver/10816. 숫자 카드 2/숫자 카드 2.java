ab1740ee422b491194791bc02be56b0787414405
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-02-01 21:49:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1576 ms, Memory: 123588 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,69 +1,95 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    //이분탐색으로 구현
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int N = sc.nextInt();
+    private static BufferedReader br;
+    private static StringBuilder sb;
+    private static StringTokenizer st;
+    private static int n,m;
+    private static int[] arr;
 
-        int[]arr =new int[N];
-        for(int i = 0 ; i < N; i++){
-            arr[i] = sc.nextInt();
+    public static void input() throws Exception{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-//        System.out.println(Arrays.toString(arr));
-        int M = sc.nextInt();
 
-        for(int i = 0 ; i <M ; i++){
-            int key = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
 
-            sb.append(upperIndex(arr,key)-lowerIndex(arr,key)).append(' ');
+        m = Integer.parseInt(st.nextToken());
 
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < m ; i++){
+            process(Integer.parseInt(st.nextToken()));
         }
+
+
+
+    }
+
+    public static void process(int x){
+        int leftS = 0;
+        int leftE = n-1;
+
+        int rightS =0;
+        int rightE = n-1;
+
+        int leftIndex=-1;
+        int rightIndex=-1;
+
+        boolean foundLeft = false;
+        boolean foundRight = false;
+
+        //leftIndex x를 포함하며 제일 왼쪽
+        while(leftS<=leftE){
+            int mid = (leftS+leftE)/2;
+            if(arr[mid]>x){
+                leftE = mid-1;
+            }else if(arr[mid]<x){
+                leftS = mid+1;
+            }else{
+                leftIndex = mid;
+                leftE= mid-1;
+                foundLeft = true;
+            }
+        }
+
+        //rightIndex x를 포함하여 제일 오른쪽
+        while(rightS<=rightE){
+            int mid = (rightS+rightE)/2;
+            if(arr[mid]>x){
+                rightE = mid-1;
+            }else if(arr[mid]<x){
+                rightS = mid+1;
+            }else{
+                rightIndex = mid;
+                rightS= mid+1;
+                foundRight = true;
+            }
+        }
+
+
+        if (!foundLeft || !foundRight) {
+            sb.append(0).append(" ");
+        } else {
+            sb.append(rightIndex - leftIndex + 1).append(" ");
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        input();
         System.out.println(sb);
-
-
-    }
-
-    private static int lowerIndex(int[] arr, int key) {
-        int high = arr.length;
-        int low = 0;
-        while (low<high){
-            int mid = (high + low)/2;
-
-            if(key <=arr[mid]){
-                high = mid;
-            }
-            else {
-                low=mid+1;
-            }
-
-
-        }
-
-
-        return low;
-    }
-
-    private static int upperIndex(int[] arr, int key) {
-        int high = arr.length;
-        int low = 0;
-
-        while (low < high){
-            int mid = (high+low)/2;
-
-            if(key<arr[mid]){
-                high=mid;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-
-
-
-        return low;
     }
 }
