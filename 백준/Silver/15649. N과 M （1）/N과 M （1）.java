ae2f3d5dbb1d34821ae918d30aacb6cfa5eb98e3
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-02-16 23:04:25 +0900

    [Silver III] Title: N과 M (1), Time: 2304 ms, Memory: 62680 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N,M;
+    private static int[]arr;
+    private static boolean[] visited;
+
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visited = new boolean[N];
+    }
+
+    public static void process() {
+        recur(0);
+    }
+    public static void recur(int cur){
+        if(cur == M){
+            for(int i = 0 ; i < M; i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0 ; i <N; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            arr[cur]= i+1;
+            recur(cur+1);
+            visited[i] = false;
+        }
+
+
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+    }
+}
