3e549ffe2c64ad01aa9a5ddcc1179d7df8b5b21b
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-02-20 13:10:31 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 136 ms, Memory: 15676 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static int[] arr;
+    private static int[] operators = new int[4];
+    private static int max = Integer.MIN_VALUE;
+    private static int min = Integer.MAX_VALUE;
+
+
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            operators[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+    }
+
+    public static void process() {
+        recur(1,arr[0]);
+    }
+
+    public static void recur(int cur, int sum){
+        //1번 템플릿
+        if(cur == N){
+            max = Math.max(max,sum);
+            min = Math.min(min,sum);
+            return;
+        }
+        for(int i = 0 ; i < 4 ; i++){
+            if(operators[i]>0){
+                operators[i]--;
+                switch (i){
+                    case 0 :
+                        recur(cur+1,sum + arr[cur]);
+                        break;
+                    case 1 :
+                        recur(cur+1,sum - arr[cur]);
+                        break;
+                    case 2 :
+                        recur(cur+1,sum * arr[cur]);
+                        break;
+                    case 3 :
+                        recur( cur+1,sum / arr[cur]);
+                        break;
+                }
+                operators[i]++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
