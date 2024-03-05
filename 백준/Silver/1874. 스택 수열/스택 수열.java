57c43d0ebe7997fff1ece14b1b2d568be02f2652
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-03-05 13:31:11 +0900

    [Silver II] Title: 스택 수열, Time: 348 ms, Memory: 25368 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,37 +1,54 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static Stack<Integer> stack;
+    private static int N;
 
-        Stack<Integer> stack = new Stack<>();
 
-        int size = sc.nextInt();
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        int start = 0;
+        stack = new Stack<>();
 
-        while (size-- > 0) {
-            int input = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
 
-            if (input > start) {
-                for (int i = start + 1; i <= input; i++) {
+    }
+
+    public static void process() throws Exception{
+        int s = 0;
+
+        while(N>0){
+
+            int input = Integer.parseInt(br.readLine());
+
+            if(input>s){
+                for(int i = s+1; i<=input; i++){
                     stack.push(i);
-                    sb.append('+').append('\n');
+                    sb.append('+').append("\n");
                 }
-                start = input;
-
-            } else if (stack.peek()!=input) {
+                s = input;
+            }else if(stack.peek() != input){
                 System.out.println("NO");
                 return;
             }
             stack.pop();
-            sb.append('-').append('\n');
+            sb.append('-').append("\n");
 
+            N--;
         }
         System.out.println(sb);
+    }
 
 
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
