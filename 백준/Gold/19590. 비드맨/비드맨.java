89d1bd6a8195275f482fb03956f43959667d7445
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-03-22 12:07:28 +0900

    [Gold I] Title: 비드맨, Time: 264 ms, Memory: 22780 KB -BaekjoonHub

---
+++ 백준/Gold/19590. 비드맨/비드맨.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static long max = Long.MIN_VALUE;
+    private static long sum = 0;
+
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        
+        for(int i = 0 ; i < N ; i++){
+            long input = Long.parseLong(br.readLine());
+            sum += input;
+            max = Math.max(max,input);
+        }
+    }
+
+    public static void process() {
+        sum = sum - max;
+
+        if(sum<=max){
+            System.out.println(max-sum);
+        }else{
+            if(sum%2!=max%2){
+                System.out.println(1);
+            }else{
+                System.out.println(0);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+    }
+}
