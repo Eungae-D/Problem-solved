0d48defc5b4bc4baf4824ebb320752328ea0535d
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-11 16:26:52 +0900

    [Bronze II] Title: Farm, Time: 176 ms, Memory: 16684 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.java
+++ 백준/Bronze/16283. Farm/Farm.java
@@ -1,30 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int n = sc.nextInt();
-        int w = sc.nextInt();
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int A,B,N,W;
 
+
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+    }
+
+    public static void process() {
         int count = 0;
-        int sheep = 0;
-        int cow = 0;
-        for(int i = 1; i < 1000; i++){
-            for(int j = 1; j <1000 ; j++){
-                if(i+j==n && i*a+j*b==w){
-                    sheep = i;
-                    cow = j;
+
+        for(int i = 1; i <= 1000 ; i++) {
+            for(int j = 1 ; j <= 1000 ; j++){
+
+                if(i+j == N && i*A + j*B == W){
                     count++;
+                    sb.append(i+" "+j);
                 }
             }
         }
-        if(count==0||count>=2){
+
+        if(count != 1){
             System.out.println(-1);
         }else{
-            System.out.println(sheep+" "+cow);
+            System.out.println(sb);
         }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
