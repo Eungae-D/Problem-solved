947b9ea811d5f06823a9483179bc20b19a3d95c5
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-11 13:00:19 +0900

    [Bronze III] Title: 통학의 신, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,16 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static StringBuilder sb;
+    private static int A,B;
 
 
-        for(int i = -1000 ; i <= 1000; i++){
-            if(Math.pow(i,2)+2*a*i+b==0){
-                System.out.print(i+" ");
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+    }
+
+    public static void process() {
+        for(int i = -1000; i <= 1000 ; i++){
+            if(Math.pow(i,2) + 2*A*i + B == 0){
+                sb.append(i).append(" ");
             }
         }
+        System.out.println(sb);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
