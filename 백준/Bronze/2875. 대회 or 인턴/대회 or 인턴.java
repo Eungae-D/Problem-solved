696e1ed7d6e01162a6b5f548fa646cbf5fcf575b
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-13 11:11:36 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -1,19 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N,M,K;
+    private static int answer = 0;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int k = sc.nextInt();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-        int team = 0;
-        while(n >= 2 && m >= 1 && n+m-k >= 3){
-            team++;
-            n = n-2;
-            m = m-1;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+    }
+
+    //실행
+    public static void process() {
+        while( N >= 2 && M >=1 && N + M - K >=3){
+            answer++;
+            N = N-2;
+            M = M-1;
         }
-        System.out.println(team);
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception{
+        input();
+        process();
     }
 }
