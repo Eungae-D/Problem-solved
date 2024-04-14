93f266bea532d77c1579daf93423962d9366f19e
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-14 11:12:52 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 152 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -3,35 +3,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int[] arr;
+    private static int answer = 0;
 
-        int[] arr = new int[5];
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
+        arr = new int[5];
         for(int i = 0 ; i < 5 ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+    }
 
-        int count = 0;
-        int answer = 0;
+    //실행
+    public static void process() {
+        outer : for(int i = 1 ; i < 1000000; i++){
+            int count = 0;
 
-        while (true){
-
-            count = 0;
-            answer++;
-
-            for(int i = 0 ; i < 5; i++){
-                if(answer%arr[i]==0){
+            for(int j = 0 ; j < 5 ; j++){
+                if(i%arr[j] == 0 ){
                     count++;
                 }
             }
+
             if(count>2){
+                answer = i;
                 break;
             }
-
         }
         System.out.println(answer);
+    }
 
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
