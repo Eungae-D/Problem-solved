dfc0bd9addf53e1a9c130cd5ac5bf6cb14efe32d
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-14 13:29:01 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PrimitiveIterator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,33 +7,33 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N;
-    private static double[] arr;
-    private static double maxScore = Integer.MIN_VALUE;
-    private static double answer;
+    private static int[]arr;
+    private static double sum = 0;
+    private static double answer = 0;
+    private static double max = Integer.MIN_VALUE;
 
-
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
 
-        arr = new double[N];
-
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N ; i++){
+
+        for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            maxScore = Math.max(maxScore,arr[i]);
+            max = Math.max(max, arr[i]);
         }
     }
 
+    //실행
     public static void process() {
-        for(int i = 0 ; i < N ; i++){
-            answer += (arr[i]/maxScore)*100;
+        for(int i = 0; i < N; i++){
+            sum += arr[i]/max *100;
         }
-        System.out.println(answer/N);
-
+        System.out.println(sum/N);
     }
 
 
