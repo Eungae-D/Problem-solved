715ccdde7c2dd64c2ad6cfb38e3705ebd57a01c7
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-15 19:02:54 +0900

    [Bronze I] Title: 암호 키, Time: 232 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.java
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -1,25 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static boolean isPrime = false;
 
-        int n = sc.nextInt();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        for(int i = 0 ; i < n ; i ++){
-            long m = sc.nextLong();
-            boolean isPrime = true;
-            for(int j = 2 ; j < 1000001; j++){
-                if(m%j == 0 ){
-                    isPrime = false;
-                    break;
-                }
-            }
-            if(!isPrime){
-                System.out.println("NO");
-            }else{
-                System.out.println("YES");
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < N ; i++){
+            long input = Long.parseLong(br.readLine());
+            isPrime = check(input);
+            process();
+        }
+    }
+
+    public static boolean check(Long input) {
+        for(int i = 2 ; i < 1000001 ; i++){
+            if(input % i == 0 ){
+                return false;
             }
         }
+        return true;
+    }
+
+    public static void process(){
+        if(isPrime){
+            sb.append("YES").append("\n");
+        }else{
+            sb.append("NO").append("\n");
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        System.out.println(sb.toString());
     }
 }
