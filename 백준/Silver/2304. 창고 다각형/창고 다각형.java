5f169dc6e4c5fa871aa1f9e9bd42b9146ee2bb2d
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-16 17:35:11 +0900

    [Silver II] Title: 창고 다각형, Time: 140 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,49 +1,54 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int[] arr,prefix,suffix;
-    private static int n,ans;
+    private static int N;
+    private static int[] arr, prefix, suffix;
+    private static int answer = 0;
 
-    private static void input() throws Exception{
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
 
-        arr = new int[1002];
-        prefix = new int[1002];
-        suffix = new int[1002];
+        N = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i <n; i++){
+        arr = new int[1010];
+        prefix = new int[1010];
+        suffix = new int[1010];
+
+        for(int i = 0 ; i < N; i++){
             st = new StringTokenizer(br.readLine());
+
             int x = Integer.parseInt(st.nextToken());
             arr[x] = Integer.parseInt(st.nextToken());
         }
     }
 
-    private static void process(){
-        for(int i = 1 ; i <=1000; i++){
+    //실행
+    public static void process() {
+        for(int i = 1; i < prefix.length; i++){
             prefix[i] = Math.max(prefix[i-1],arr[i]);
         }
 
-
-        for(int i = 1000 ; i >=1; i--){
-            suffix[i] = Math.max(suffix[i+1],arr[i]);
+        for(int i = suffix.length - 2; i >=0 ; i--){
+            suffix[i] = Math.max(suffix[i+1], arr[i]);
         }
 
-
-        for(int i = 1 ; i <=1000; i++){
-            ans+=Math.min(prefix[i],suffix[i]);
+        for(int i = 1 ; i<arr.length; i++){
+            answer += Math.min(prefix[i], suffix[i]);
         }
-        
-        System.out.println(ans);
 
+        System.out.println(answer);
     }
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
