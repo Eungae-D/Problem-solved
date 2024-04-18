628d14f41a352e5263c7fdc1ee04dec14261fcee
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-18 21:47:15 +0900

    [Silver I] Title: 보석 도둑, Time: 160 ms, Memory: 15904 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.java
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -1,34 +1,46 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static long K;
+    private static int count = 0;
 
-        long k = sc.nextLong();
-        int cnt = 0;
-        ArrayList<Long> list = new ArrayList<>();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        long temp = 2;
-        while(k!=1){
-            //제곱근보다 클때까지 나누어 지는 값이 없음.(소수)
-            if(temp>=1000000){
-                list.add(k);
-                cnt++;
-                break;
-            }
-            if(k%temp==0){
-                cnt++;
-                k /=temp;
-                list.add(temp);
-            }
-            else{
-                temp++;
+        K = Long.parseLong(br.readLine());
+    }
+
+    //실행
+    public static void process() {
+        long tmp = K;
+
+        //소인수분해 구하기
+        for(long i = 2 ; i*i <K+1 ; i++){
+            while(tmp % i == 0){
+                sb.append(i+" ");
+                count++;
+                tmp /= i;
             }
         }
-        System.out.println(cnt);
-        for(int i = 0 ; i < list.size(); i++){
-            System.out.print(list.get(i)+" ");
+
+        if(tmp != 1){
+            sb.append(tmp);
+            count++;
         }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+        System.out.println(count);
+        System.out.println(sb.toString());
     }
 }
