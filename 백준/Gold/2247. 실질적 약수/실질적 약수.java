77ddfb07fce77e7b482f70fd157541b37cf3e131
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-19 14:20:01 +0900

    [Gold V] Title: 실질적 약수, Time: 756 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Gold/2247. 실질적 약수/실질적 약수.java
+++ 백준/Gold/2247. 실질적 약수/실질적 약수.java
@@ -1,17 +1,35 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static long sum = 0;
 
-        int n = input.nextInt();
 
-        long sum = 0;
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        for(int i = 2; i < n; i ++){
-            sum+=(n/i-1)*i;
+        N = Integer.parseInt(br.readLine());
+    }
+
+    //실행
+    public static void process() {
+        for(int i = 2 ; i < N ; i++){
+            sum+=(N/i-1)*i;
         }
-        System.out.println(sum%1000000);
+        sum %= 1000000;
+        System.out.println(sum);
+    }
 
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
