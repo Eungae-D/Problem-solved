fa59738573545a27acf92ac27eba359c49a16bee
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-21 17:17:23 +0900

    [Silver I] Title: 정수 좌표의 개수, Time: 184 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/16970. 정수 좌표의 개수/정수 좌표의 개수.java
+++ 백준/Silver/16970. 정수 좌표의 개수/정수 좌표의 개수.java
@@ -1,36 +1,57 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N,M,K;
+    private static int answer = 0;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int k = sc.nextInt();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
-        int ans = 0;
-        for(int x1 = 0 ; x1 <=n; x1++){
-            for(int y1 = 0 ; y1 <=m ; y1++){
-                for(int x2 = 0 ; x2<=n ; x2++){
-                    for(int y2 = 0 ; y2<=m ; y2++){
-                        if(gcd(Math.abs(x2-x1),(Math.abs(y2 - y1)))+1==k){
-                            ans++;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+    }
+    public static int gcd(int x, int y){
+        if(x == 0 || y == 0){
+            return x + y;
+        }
+
+        while(x % y != 0){
+            int temp = x % y;
+            x = y;
+            y = temp;
+        }
+        return y;
+    }
+
+    //실행
+    public static void process() {
+        for(int x1 = 0 ; x1 <= N ; x1++){
+            for(int y1 = 0 ; y1 <= M ; y1++){
+                for(int x2 = 0 ; x2 <= N ; x2++){
+                    for(int y2 = 0 ; y2 <= M ; y2++){
+                        if(gcd(Math.abs(x2-x1), Math.abs(y2-y1)) + 1 == K){
+                            answer++;
                         }
                     }
                 }
             }
         }
-        System.out.println(ans/2);
+        System.out.println(answer/2);
     }
-    public static int gcd(int a, int b){
-        if(a==0||b == 0){
-            return a + b;
-        }
-        while(a%b!=0){
-            int temp = a%b;
-            a= b;
-            b = temp;
-        }
-        return b;
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
