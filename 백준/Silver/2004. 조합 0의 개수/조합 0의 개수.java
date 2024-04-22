5e51b0724a6c304cfa18140f61c2e1ce66e5744f
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-22 11:20:47 +0900

    [Silver II] Title: 조합 0의 개수, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -1,40 +1,51 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static long N,M;
 
-        long n = sc.nextLong();
-        long m = sc.nextLong();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
-        long fiveCnt = fiveCount(n)-fiveCount(n-m)-fiveCount(m);
-        long twoCnt = twoCount(n)-twoCount(n-m)-twoCount(m);
-
-        System.out.println(Math.min(fiveCnt,twoCnt));
+        N = Long.parseLong(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+    }
+    public static long cnt5(long input){
+        long temp = 0;
+        while(input >= 5){
+            temp += input/5;
+            input /=5;
+        }
+        return temp;
     }
 
-    public static long fiveCount(long input){
-        long count = 0 ;
-
-        for(long i = 5 ; i <=input; i *= 5){
-            if(input ==0){
-                return 0;
-            }
-            count += input/i;
+    public static long cnt2(long input){
+        long temp = 0;
+        while(input >= 2){
+            temp += input/2;
+            input /=2;
         }
-
-        return count;
+        return temp;
     }
 
-    public static long twoCount(long input){
-        long count = 0 ;
+    //실행
+    public static void process() {
+        long countFive = cnt5(N) - cnt5(N-M) - cnt5(M);
+        long countTwo = cnt2(N) - cnt2(N-M) - cnt2(M);
 
-        for(long i = 2 ; i <=input; i *= 2){
-            if(input ==0){
-                return 0;
-            }
-            count += input/i;
-        }
-        return count;
+        System.out.println(Math.min(countFive,countTwo));
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
