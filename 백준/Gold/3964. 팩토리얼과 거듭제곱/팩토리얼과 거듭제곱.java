390fc57c2c5bd03b40da0725e5bac44621e99d42
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-04-22 12:37:28 +0900

    [Gold II] Title: 팩토리얼과 거듭제곱, Time: 240 ms, Memory: 19780 KB -BaekjoonHub

--- 백준/Gold/3964. 팩토리얼과 거듭제곱/팩토리얼과 거듭제곱.java
+++ 백준/Gold/3964. 팩토리얼과 거듭제곱/팩토리얼과 거듭제곱.java
@@ -1,60 +1,56 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static boolean[] notPrime;
-    static ArrayList<Long> primes;
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int T;
+    private static long N,K;
+    private static boolean[] isPrime = new boolean[1000010];
+    private static ArrayList<Long> primes = new ArrayList<Long>();
+    public static void eratosthenes(){
+        isPrime[0] = isPrime[1] = false;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        for(long i = 2; i <= 1000000 ; i++){
+            if(!isPrime[(int)i]) continue;
 
+            primes.add(i);
 
-        int t = sc.nextInt();
-
-        notPrime = new boolean[1000001];
-        primes = new ArrayList<>();
-
-        notPrime[0] = true;
-        notPrime[1] = true;
-
-        for(int i = 2; i < notPrime.length; i++){
-            if(notPrime[i]) continue;
-
-            primes.add((long)i);
-
-            for(int j = i*i; j< notPrime.length && j>0; j+=i){
-                notPrime[j]=true;
+            for(long j = i*i ; j <= 1000000 && j > 0; j+=i){
+                isPrime[(int)j] = false;
             }
-        }
-
-        for(int tc = 0 ; tc<t; tc++){
-            long n = sc.nextLong();
-            long k = sc.nextLong();
-
-
-            Map<Long,Integer> primeCheck = primeCount(k);
-            long result = Long.MAX_VALUE;
-
-            for(Map.Entry<Long,Integer> entry : primeCheck.entrySet()){
-                long prime = entry.getKey();
-                int count = entry.getValue();
-                result = Math.min(result,countFactorial(n,prime)/count);
-            }
-            System.out.println(result);
-
-
-
-
         }
     }
-    public static Map<Long, Integer> primeCount(long k) {
-        Map<Long, Integer> count = new HashMap<>();
+
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        T = Integer.parseInt(br.readLine());
+
+        Arrays.fill(isPrime, true);
+        eratosthenes();
+
+        for(int tc = 0 ; tc < T ; tc++){
+            st = new StringTokenizer(br.readLine());
+
+            N = Long.parseLong(st.nextToken());
+            K = Long.parseLong(st.nextToken());
+
+            process();
+        }
+    }
+    public static Map<Long,Integer> primeCount(long k){
+        Map<Long,Integer> count = new TreeMap<>();
+
         for(long prime : primes){
             if(prime * prime > k) break;
+
             int cnt = 0;
-            while(k%prime ==0){
+            while(k%prime == 0){
                 cnt++;
                 k/=prime;
             }
@@ -65,15 +61,35 @@ public class Main {
         if(k>1){
             count.put(k,1);
         }
+
         return count;
     }
-
-    public static long countFactorial(long n, long prime) {
+    public static long countFactorial(long n, long prime){
         long count = 0;
-        while (n > 0) {
-            count += n / prime;
+        while(n >= prime){
+            count += n/prime;
             n /= prime;
         }
         return count;
+    }
+
+    //실행
+    public static void process() {
+        Map<Long,Integer> primeCheck = primeCount(K);
+        long min = Long.MAX_VALUE;
+
+        for(Map.Entry<Long,Integer> entry : primeCheck.entrySet()){
+            long prime = entry.getKey();
+            int cnt = entry.getValue();
+
+            min = Math.min(min, countFactorial(N,prime)/cnt);
+        }
+        sb.append(min).append("\n");
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        System.out.println(sb);
     }
 }
