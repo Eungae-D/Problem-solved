819fe61e34f205d26c2517c2706511d6b9ab5ab2
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-02 12:02:42 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1436 ms, Memory: 128676 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,29 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static int N;
+    private static ArrayList<Integer> list = new ArrayList<Integer>();
 
-        int N = sc.nextInt();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        ArrayList<Integer> list = new ArrayList<>();
+        N = Integer.parseInt(br.readLine());
 
-
-
-        for(int i = 0 ; i <N; i++){
-            list.add(sc.nextInt());
+        for(int i = 0 ; i < N ; i++){
+            list.add(Integer.parseInt(br.readLine()));
         }
+    }
 
+    //실행
+    public static void process() {
         Collections.sort(list);
 
-        for(int output : list){
-            sb.append(output).append("\n");
+        for(int answer : list){
+            sb.append(answer).append("\n");
         }
+
         System.out.println(sb);
+    }
 
 
-
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
