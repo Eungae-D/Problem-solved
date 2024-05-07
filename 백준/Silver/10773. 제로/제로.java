cac03f7df4c2a14468744216511f517973c114ca
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-07 11:20:08 +0900

    [Silver IV] Title: 제로, Time: 260 ms, Memory: 23948 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,30 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static Stack<Integer> stack = new Stack<>();
+    private static int sum = 0;
 
-        Stack<Integer> stack = new Stack<>();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        int sum = 0;
-        for(int i = 0 ; i < K; i++){
-            int input = sc.nextInt();
-            if(input!=0){
-                stack.push(input);
-            }
-            else {
-                if(stack.isEmpty()){
-                    System.out.println(0);
-                    return;
-                }
-                stack.pop();
-            }
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0 ; i < N ; i++){
+            int input = Integer.parseInt(br.readLine());
+
+            stackProcess(input);
         }
+        process();
+    }
+
+    //스택 쌓기 실행
+    public static void stackProcess(int input) {
+        if(input==0){
+            stack.pop();
+            return;
+        }
+
+        stack.push(input);
+    }
+
+    //출력
+    public static void process(){
         while (!stack.isEmpty()){
             sum+=stack.pop();
         }
+
         System.out.println(sum);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
     }
 }
