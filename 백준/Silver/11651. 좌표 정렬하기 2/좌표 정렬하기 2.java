a56b67f45d075d69602ff056476936d363b46278
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-09 20:50:28 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 712 ms, Memory: 53656 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,46 +1,62 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static class Node implements Comparable<Node> {
-		int x;
-		int y;
+    static class Node implements Comparable<Node>{
+        int x;
+        int y;
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
 
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+        @Override
+        public int compareTo(Node o){
+            if(this.y == o.y){
+                return this.x - o.x;
+            }
 
-		@Override
-		public int compareTo(Node o) {
-			if (this.y == o.y) {
-				return this.x - o.x;
-			}
-			return this.y - o.y;
-		}
+            return this.y-o.y;
+        }
+    }
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static PriorityQueue<Node> queue = new PriorityQueue<>();
 
-	}
 
-	// 노드만들어서 comparable이용하는방법으로
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-		int size = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
 
-		PriorityQueue<Node> q = new PriorityQueue<>();
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < size; i++) {
-			int nodex = sc.nextInt();
-			int nodey = sc.nextInt();
-			q.add(new Node(nodex, nodey));
-		}
-		
-		while(!q.isEmpty()) {
-			Node ans = q.poll();
-			System.out.println(ans.x+" "+ans.y);
-		}
+            queue.add(new Node(x,y));
+        }
+    }
 
-	}
+    //실행
+    public static void process() {
+        while (!queue.isEmpty()){
+            Node n = queue.poll();
+            sb.append(n.x+" "+n.y).append("\n");
+        }
+        System.out.println(sb);
+    }
 
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+    }
 }
