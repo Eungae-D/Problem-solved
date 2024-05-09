2c626cc33a07cff2b64dc63b6db9bb6c5ef20e04
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-09 20:35:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 784 ms, Memory: 54448 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static int[][] arr;
+
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    //실행
+    public static void process() {
+        Arrays.sort(arr, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                if(o1[0] == o2[0]){
+                    return o1[1]-o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        for(int i = 0 ; i < N ; i++){
+            sb.append(arr[i][0] +" "+arr[i][1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+    }
+}
