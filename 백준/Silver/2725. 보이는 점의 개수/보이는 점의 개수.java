af3a937f333234f60cc6c13f94a8f7ec470efb32
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-14 11:44:56 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 172 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
@@ -1,35 +1,57 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int T,N;
+    private static int[] arr = new int[1010];
 
-        int c = sc.nextInt();
-
-        int []arr = new int[1001];
-        arr[1] = 3;
-
-        for(int i = 2; i <=1000; i++){
-            int cnt = 0;
-            for(int j = 1; j<i; j++){
-                if(gcd(i,j)==1){
-                    cnt+=2;
-                }
-                arr[i] = arr[i-1]+cnt;
-            }
-        }
-
-        for(int i = 0; i < c; i++){
-            System.out.println(arr[sc.nextInt()]);
-        }
-    }
-    
-    public static int gcd(int i, int j) {
-        while (i%j!=0){
+    //최대공약수
+    public static int gcd(int i, int j){
+        while(i%j != 0){
             int temp = i%j;
             i = j;
             j = temp;
         }
         return j;
+    }
+
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        T = Integer.parseInt(br.readLine());
+
+        arr[1] = 3;
+        for(int i = 2 ; i <= 1000 ; i++){
+            int cnt = 0;
+            for(int j = 1 ; j < i ; j++){
+                if(gcd(i,j) == 1){
+                    cnt+=2;
+                }
+            }
+            arr[i] = arr[i-1]+cnt;
+        }
+
+        for(int tc = 0 ; tc < T ; tc++){
+            N = Integer.parseInt(br.readLine());
+
+            process();
+        }
+    }
+
+    //실행
+    public static void process() {
+        sb.append(arr[N]).append("\n");
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        System.out.println(sb);
     }
 }
