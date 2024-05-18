14473afb407cb4e6a1348ffa86531972412e58ef
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-18 21:57:34 +0900

    [Gold V] Title: 소수인팰린드롬, Time: 1748 ms, Memory: 111976 KB -BaekjoonHub

--- 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
+++ 백준/Gold/1990. 소수인팰린드롬/소수인팰린드롬.java
@@ -1,52 +1,74 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        boolean[] isPrime = new boolean[100000001];
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int A,B;
+    private static boolean[] isPrime = new boolean[100000010];
 
-        for(int i = 0; i <isPrime.length; i++){
-            isPrime[i] = true;
-        }
-
-        isPrime[0] = false;
-        isPrime[1] = false;
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        for(int i = 2 ; i *i <=100000000; i++){
+    //소수 체크
+    public static void primeCheck(){
+        for(int i = 2 ; i * i <= 100000000 ; i++){
             if(isPrime[i]){
-                for(int j = i* i ; j<=100000000; j+=i){
+                for(int j = i*i ; j <= 100000000 ; j+=i){
                     isPrime[j] = false;
                 }
             }
         }
-
-        for(int i = a ;  i <=b; i++){
-            if(isPrime[i] && isPalindrome(i)){
-                sb.append(i).append("\n");
-            }
-        }
-        sb.append(-1);
-        System.out.println(sb);
     }
 
-    public static boolean isPalindrome(int i) {
-        int input = i;
-        int reverseNumber = 0;
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        while( i!=0){
-            int lastNumber = i%10;
-            reverseNumber = reverseNumber*10+lastNumber;
-            i /=10;
+        Arrays.fill(isPrime,true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        primeCheck();
+
+        st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+    }
+    //펠린드롬 체크(sb.reverse() 대신 구현)
+    public static boolean palindromeCheck(int i){
+        int input = i;
+        int reverseNum = 0;
+
+        while (i != 0){
+            int lastNum = i%10;
+            reverseNum = reverseNum*10+lastNum;
+            i/=10;
         }
-        if(input ==reverseNumber){
+        if(input == reverseNum){
             return true;
         }else{
             return false;
         }
     }
 
+    //실행
+    public static void process() {
+        for(int i = A ; i <= B ; i++){
+            if(isPrime[i] && palindromeCheck(i)){
+                sb.append(i).append("\n");
+            }
+        }
+        sb.append(-1).append("\n");
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+        System.out.println(sb.toString());
+    }
 }
