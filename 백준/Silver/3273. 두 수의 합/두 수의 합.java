1e12185c300cf185d367f04218f4875590011000
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-25 00:25:30 +0900

    [Silver III] Title: 두 수의 합, Time: 440 ms, Memory: 31868 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,42 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N;
+    private static int X;
+    private static int[] arr;
+    private static int count = 0;
 
-        Scanner sc = new Scanner(System.in);
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        int n = sc.nextInt();
+        N = Integer.parseInt(br.readLine());
 
-        int []arr = new int[n];
+        arr = new int[N];
 
-        for(int i = 0 ; i < arr.length; i++){
-            arr[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int x = sc.nextInt();
-
         Arrays.sort(arr);
 
-        int s = 0;
-        int e = n-1;
-
-
-        int cnt = 0;
-
-        while (s<e){
-            if(arr[s]+arr[e]==x){
-                cnt++;
-                s +=1;
-                e -=1;
-            } else if (arr[s]+arr[e]<x) {
-                s +=1;
-            }else{
-                e -=1;
-            }
-        }
-        System.out.println(cnt);
-
+        X = Integer.parseInt(br.readLine());
     }
 
+    //실행
+    public static void process() {
+        int s = 0;
+        int e = N-1;
+
+
+        while(s<e){
+            if(arr[s]+arr[e]==X){
+                count++;
+                s++;
+                e--;
+            }else if(arr[s]+arr[e]<X){
+                s++;
+            }else{
+                e--;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
+    }
 }
