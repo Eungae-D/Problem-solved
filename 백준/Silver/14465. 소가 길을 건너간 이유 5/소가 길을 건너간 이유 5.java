34e15de755bddda086d1f2331adb1ab915b6fab9
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-26 20:51:12 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 144 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.java
@@ -1,53 +1,61 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N,K,B;
+    private static boolean[] arr;
+    private static int count = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int b = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
 
-
-        //정상 false 고장 true
-        boolean[] arr = new boolean[n+1];
-
-        int count = 0 ;
-
-        for(int i = 1 ; i <=b; i++){
-            int index = sc.nextInt();
-            arr[index] = true;
+        arr = new boolean[N+1];
+        
+        for(int i = 1 ; i <=B ; i++){
+            arr[Integer.parseInt(br.readLine())] = true;
         }
+    }
 
-        //초기 k만큼 고장 갯수
-        for(int i = 1 ; i <k+1; i++){
-            if(arr[i]){
-                count++;
-            }
+    //실행
+    public static void process() {
+        for(int i = 1 ; i <=K ; i++){
+            if(arr[i]) count++;
         }
 
         int s = 1;
-        int e = k;
+        int e = K;
 
         int min = Integer.MAX_VALUE;
 
-        while(e<n){
-            if(arr[s]&& !arr[e+1]){
+        while(e<N){
+            if(arr[s] && !arr[e+1]){
                 count--;
-            }else if(!arr[s]&& arr[e+1] ){
+            }else if(!arr[s] && arr[e+1]){
                 count++;
             }
-            min = Math.min(min, count);
-
+            min = Math.min(min,count);
             s++;
             e++;
+
         }
 
         System.out.println(min);
+    }
 
 
-
-
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
