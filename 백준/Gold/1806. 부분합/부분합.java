f37a400fea27ac0b17bc546b79c32be5ae305b5b
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-27 18:11:14 +0900

    [Gold IV] Title: 부분합, Time: 304 ms, Memory: 23844 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,43 +1,61 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N,S;
+    private static int[]arr;
+    private static int count = 0;
+    private static int min = Integer.MAX_VALUE;
 
-        int n = sc.nextInt();
-        int s = sc.nextInt();
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        int[] arr = new int[n+1];
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
 
-        for(int i = 0 ; i < n; i ++){
-            arr[i] = sc.nextInt();
+        arr = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+    }
 
-        int start = 0;
-        int end = 0;
-
-        int answer = Integer.MAX_VALUE;
+    //실행
+    public static void process() {
+        int s = 0 ;
+        int e = 0 ;
         int sum = 0;
 
-        while(start<=n && end <=n){
-
-            if(sum<s){
-                sum+=arr[end];
-                end++;
+        while(s <= N && e <= N){
+            if(sum<S){
+                sum+=arr[e];
+                e++;
             }else{
-                sum-=arr[start];
-                start++;
+                sum-=arr[s];
+                s++;
             }
 
-            if(sum>=s){
-                answer = Math.min(answer,end-start);
+            if(sum>=S){
+                min = Math.min(min,e-s);
             }
-
         }
-        if(answer==Integer.MAX_VALUE){
+
+        if(min == Integer.MAX_VALUE){
             System.out.println(0);
-        }else{
-            System.out.println(answer);
-        }
+        }else System.out.println(min);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
