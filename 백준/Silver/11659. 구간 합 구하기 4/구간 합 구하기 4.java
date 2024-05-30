a8af7922a3b03d9076404ea1a6704f1403c744e0
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-30 13:21:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 51440 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,45 +1,50 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int n, m, i, j;
-    private static int[] arr;
+    private static int N,M;
+    private static int[]arr, prefix;
 
-
-    public static void input() throws Exception{
-        sb = new StringBuilder();
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
         st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n+1];
-        st= new StringTokenizer(br.readLine());
+        arr = new int[N+1];
+        prefix = new int[N+1];
 
-        for(int i = 1 ; i <= n; i++){
-            arr[i] =  arr[i-1]+Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1 ; i <= N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            prefix[i] = prefix[i-1]+arr[i];
         }
-    }
 
-    public static void process() throws Exception{
-        for(int i = 0 ; i < m ; i++){
+        for(int tc = 0 ; tc < M ; tc++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            sb.append(arr[end]-arr[start-1]+"\n");
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            process(i,j);
         }
-        System.out.println(sb);
 
     }
 
+    //실행
+    public static void process(int i, int j) {
+        sb.append(prefix[j]-prefix[i-1]).append("\n");
+    }
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         input();
-        process();
+        System.out.println(sb.toString());
     }
 }
