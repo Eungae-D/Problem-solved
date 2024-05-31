e7db7fc08c91d9fcebb655dc507f8b849d27b770
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-31 18:53:40 +0900

    [Gold IV] Title: 점수따먹기, Time: 1200 ms, Memory: 20744 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -1,63 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
-    private static int n,m, ans;
-    private static int[][] arr;
-    private static int[][] prefix;
+    private static int N,M;
+    private static int[][] arr = new int[200][200];
+    private static int[][] prefix = new int[200][200];
+    private static int answer = Integer.MIN_VALUE;
 
 
-    private static void input() throws Exception{
+
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
-        arr = new int[200][200];
-        prefix = new int[200][200];
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for(int i = 1; i<=n; i++){
+        for(int i = 1 ; i <= N ; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1 ; j<=m ; j++){
+            for(int j = 1; j <= M ; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i = 1; i <=n; i++){
-            for(int j = 1; j <=m ; j++){
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1; j <= M ; j++){
                 prefix[i][j] += arr[i][j]+prefix[i][j-1]+prefix[i-1][j]-prefix[i-1][j-1];
-//                System.out.print(prefix[i][j]+" ");
             }
-//            System.out.println();
         }
     }
 
-    private static void process() {
-
-        ans = Integer.MIN_VALUE;
-        
-        for(int x1 = 1 ; x1<=n; x1++){
-            for(int y1 = 1 ; y1<=m; y1++){
-                for(int x2 = x1; x2<=n; x2++){
-                    for(int y2 = y1; y2<=m; y2++){
-                        ans = Math.max(ans,prefix[x2][y2]-prefix[x1-1][y2]-prefix[x2][y1-1]+prefix[x1-1][y1-1]);
+    //실행
+    public static void process() {
+        for(int x1 = 1 ; x1 <= N ; x1++){
+            for(int y1 = 1 ; y1 <= M ; y1++){
+                for(int x2 = x1 ; x2 <= N ; x2++){
+                    for(int y2 = y1 ; y2 <= M ; y2++){
+                        answer=Math.max(answer, prefix[x2][y2] - prefix[x2][y1-1] - prefix[x1-1][y2] + prefix[x1-1][y1-1]);
                     }
                 }
             }
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 
 
-
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
