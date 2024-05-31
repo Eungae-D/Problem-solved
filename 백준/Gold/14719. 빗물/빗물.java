dcd4bb3035d433c4ab4df26b62cd858d769626dc
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-05-31 13:04:41 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -3,53 +3,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
-    private static int h,w,water;
-    private static int[] blocks,prefix,suffix;
+    private static int H,W;
+    private static int[] arr, prefix,suffix;
+    private static int rain;
+    private static int answer;
 
-    private static void input() throws Exception{
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
 
-        blocks = new int[502];
-        prefix = new int[502];
-        suffix = new int[502];
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
 
-        h = Integer.parseInt(st.nextToken());
-        w = Integer.parseInt(st.nextToken());
+        arr = new int[510];
+        prefix = new int[510];
+        suffix = new int[510];
 
         st = new StringTokenizer(br.readLine());
-
-        for(int i = 1; i<=w; i++){
-            blocks[i] = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= W ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            rain += arr[i];
         }
     }
 
-    private static void process(){
-        for(int i = 1; i <=w; i++){
-            prefix[i] = Math.max(prefix[i-1],blocks[i]);
+    //실행
+    public static void process() {
+        for(int i = 1 ; i < prefix.length ; i++){
+            prefix[i] = Math.max(prefix[i-1],arr[i]);
         }
 
-        for(int i = w; i>=1; i--){
-            suffix[i] = Math.max(suffix[i+1],blocks[i]);
+        for(int i = suffix.length-2 ; i >= 1 ; i--){
+            suffix[i] = Math.max(suffix[i+1],arr[i]);
         }
 
-        for(int i = 0 ; i < blocks.length;i++){
-            water+= Math.min(prefix[i],suffix[i]);
+        for(int i = 1 ; i < arr.length; i++){
+            answer+=Math.min(prefix[i],suffix[i])-arr[i];
         }
-
-        for(int i = 0 ; i < blocks.length;i++){
-            water-=blocks[i];
-        }
-
-        System.out.println(water);
-
-
+        System.out.println(answer);
     }
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
