fd768d9cad2cf22861366f32d7ea6b73af26a1d2
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-04 22:01:44 +0900

    [Gold V] Title: 개똥벌레, Time: 328 ms, Memory: 30812 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -1,62 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
-    private static int n,h,cnt;
-    private static int[] arr,prefix;
+    private static int N,H;
+    private static int[]arr,prefix;
+    private static int min = Integer.MAX_VALUE;
+    private static int count = 0;
 
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
-        n = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
-        arr = new int[500001];
-        prefix = new int[500001];
+        st = new StringTokenizer(br.readLine());
 
-        for(int i = 0 ; i < n ; i++){
-            st = new StringTokenizer(br.readLine());
-            int temp = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-            if(i % 2 == 0){
-                arr[h-temp+1]++;
-            }else{
+        arr = new int[H+1];
+        prefix = new int[H+1];
+        for(int i = 0 ; i < N ; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(i%2==1){
                 arr[1]++;
-                arr[temp+1]--;
+                arr[num+1]--;
+            }else{
+                arr[H-num+1]++;
             }
         }
-
     }
 
-
+    //실행
     public static void process() {
-
-        for(int i = 1 ; i <= h; i++){
+        for(int i = 1; i <= H ; i++){
             prefix[i] = prefix[i-1]+arr[i];
-        }
-
-        int min = Integer.MAX_VALUE;
-        for(int i = 1; i <= h; i++){
             min = Math.min(min,prefix[i]);
         }
 
-        cnt = 0;
-
-        for(int i = 1; i <= h; i++){
-            if(prefix[i] ==min){
-                cnt++;
+        for(int i = 1; i <= H ; i++){
+            if(prefix[i] == min){
+                count++;
             }
         }
-        System.out.println(min+" "+cnt);
 
+        System.out.println(min+" "+count);
     }
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
