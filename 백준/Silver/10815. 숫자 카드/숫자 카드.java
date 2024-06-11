c8fe6e4f2228c98708cb0054b2e2305052ed017a
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-11 11:05:33 +0900

    [Silver V] Title: 숫자 카드, Time: 1324 ms, Memory: 106020 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,60 +4,57 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static BufferedReader br;
     private static StringBuilder sb;
+    private static BufferedReader br;
     private static StringTokenizer st;
-    private static int n,m;
+    private static int N,M;
     private static int[] arr;
 
-    public static void input() throws Exception{
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
         st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-
-        arr = new int[n];
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
-        
-        st = new StringTokenizer(br.readLine());
-        m= Integer.parseInt(st.nextToken());
 
+        Arrays.sort(arr);
+
+        M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < m ; i++){
+        for(int i = 0 ; i < M ; i++){
             process(Integer.parseInt(st.nextToken()));
         }
-
-
     }
 
-    public static void process(int x){
+    //실행
+    public static void process(int input) {
         int s = 0;
-        int e = n-1;
-        int ans = 0;
+        int e = N-1;
 
-        while(s<=e){
+        while (s<=e){
             int mid = (s+e)/2;
 
-            if(arr[mid]==x){
-                ans = 1;
-                break;
-            } else if (arr[mid]<x) {
+            if(arr[mid]<input){
                 s = mid+1;
-            }else{
+            }else if(arr[mid]==input){
+                sb.append(1+" ");
+                return;
+            }else if(arr[mid]>input){
                 e = mid-1;
             }
         }
-        System.out.print(ans+" ");
-
+        sb.append(0+" ");
     }
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         input();
+        System.out.println(sb.toString());
     }
 }
