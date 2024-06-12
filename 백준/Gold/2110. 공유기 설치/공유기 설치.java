32557ecb3aeee20217f69d457262978a3394b05a
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-12 19:33:05 +0900

    [Gold IV] Title: 공유기 설치, Time: 316 ms, Memory: 28836 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -8,58 +8,57 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N,C;
-    private static int[]arr;
+    private static int[] arr;
+    private static int answer;
 
-    public static void input() throws Exception{
-        sb = new StringBuilder();
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        N= Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        arr= new int[N];
-
-        for(int i = 0 ; i < N;  i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         Arrays.sort(arr);
-
     }
 
-    public static void process(){
-        int answer = 0;
+    //실행
+    public static void process() {
         int s = 0;
-        int e = arr[N-1]+arr[0];
+        int e = arr[N-1];
 
         while(s<=e){
             int cnt = 1;
-
-            int beforeHouse = arr[0];
+            int before = arr[0];
 
             int mid = (s+e)/2;
 
-            for(int i = 1; i < N ; i++){
-                if(arr[i]-beforeHouse>=mid){
+            for(int i = 1 ; i < N ; i++){
+                if(arr[i]-before>=mid){
                     cnt++;
-                    beforeHouse = arr[i];
+                    before = arr[i];
                 }
             }
 
             if(cnt>=C){
                 answer = mid;
-
                 s = mid+1;
             }else{
                 e = mid-1;
             }
+
         }
         System.out.println(answer);
-
     }
-    public static void main(String[] args) throws Exception{
+
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
