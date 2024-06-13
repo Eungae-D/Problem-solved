33bcaa99cec01d1921c5c5ac1be3e3233a02377f
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-13 16:03:16 +0900

    [Gold II] Title: 숫자구슬, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Gold/2613. 숫자구슬/숫자구슬.java
+++ 백준/Gold/2613. 숫자구슬/숫자구슬.java
@@ -1,77 +1,92 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n, m;
-    public static int[] arr;
-    public static int s, e;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+    private static StringBuilder sb;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int N,M;
+    private static int[] arr;
+    private static int s = Integer.MIN_VALUE ,e = 0;
+    private static int min;
+    private static int marbleCount;
 
-        arr = new int[n];
+    //입력
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            s = Math.max(s, arr[i]);
+            s = Math.max(s,arr[i]);
             e += arr[i];
         }
-
-
-
-        int mid = 0;
-        while (s <= e){
-            mid = (s + e) / 2;
-            int cnt = check(mid);
-            if(cnt > m) {
-                s = mid+1;
-            } else {
-                e = mid-1;
-            }
-        }
-        sb.append(s).append("\n");
-
-
-        int cnt = 0;
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            sum += arr[i];
-            if(sum > s){
-                m--;
-                sum = arr[i];
-                sb.append(cnt).append(" ");
-                cnt = 1;
-            } else {
-                cnt++;
-            }
-           
-            if(m == n-i) break;
-        }
-
-        for(int i = m ; i >0; i--){
-            sb.append(cnt).append(" ");
-            cnt = 1;
-        }
-        System.out.println(sb.toString());
     }
-
-
-
-    public static int check(int mid){
-        int sum = 0;
+    public static int searchMarbleGroup(int boundary){
         int cnt = 1;
-        for(int i=0;i<n;i++){
-            sum += arr[i];
-            if(sum > mid){
+        int sum = 0;
+
+        for(int i = 0 ; i < N ; i++){
+            sum+=arr[i];
+            if(sum>boundary){
                 cnt++;
                 sum = arr[i];
             }
         }
         return cnt;
+    }
+
+    //실행
+    public static void process() {
+        while(s<=e){
+            int mid = (s+e)/2;
+
+            int count = searchMarbleGroup(mid);
+
+            if(count>M){
+                s = mid+1;
+            }else{
+                min = mid;
+                e = mid-1;
+            }
+        }
+        sb.append(min).append("\n");
+
+        int sum = 0;
+        int cnt = 0;
+        for(int i = 0 ; i < N ; i++){
+            sum+= arr[i];
+
+            if(sum>min){
+                M--; //2
+                sum = arr[i];
+                sb.append(cnt).append(" ");
+                cnt = 1; //1
+            }else{
+                cnt++;
+            }
+
+            if(M == N-i) break;
+        }
+        for(int i = 0 ; i < M ; i++){
+            sb.append(cnt).append(" ");
+            cnt = 1;
+        }
+        System.out.println(sb);
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        input();
+        process();
     }
 }
