9f9889e620c7547f155ac297022cc556ab6b6a83
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-13 12:04:23 +0900

    [Gold V] Title: 용액, Time: 360 ms, Memory: 28992 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -8,7 +8,7 @@ public class Main {
     private static StringTokenizer st;
     private static int N;
     private static int[] arr;
-    private static long min = Long.MAX_VALUE;
+    private static int answer = Integer.MAX_VALUE;
     private static int left,right;
 
     //입력
@@ -17,45 +17,44 @@ public class Main {
         sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
-
         arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N; i++){
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+    public static void binSearch(int s, int e, int target){
+        while(s<=e){
+            int mid = (s+e)/2;
+            int sum = Math.abs(arr[mid]+target);
+
+            if(sum<answer){
+                answer = sum;
+                left = target;
+                right = arr[mid];
+            }
+
+            if(arr[mid]+target<0){
+                s = mid+1;
+            }else{
+                e = mid-1;
+            }
+
         }
     }
 
     //실행
     public static void process() {
-        int s = 0;
-        int e = N-1;
-
-        while(s<e){
-            long sum = arr[s]+arr[e];
-
-            if(Math.abs(sum)<min){
-                min = Math.abs(sum);
-                left = s;
-                right = e;
-            }
-
-            if(sum<0){
-                s++;
-            }else if(sum>0){
-                e--;
-            }else{
-                sb.append(arr[s]+" "+arr[e]);
-                return;
-            }
+        for(int i = 0 ; i < N-1; i++){
+            binSearch(i+1,N-1,arr[i]);
         }
-        sb.append(arr[left]+" "+arr[right]);
+        System.out.println(left+" "+right);
     }
 
 
     public static void main(String[] args) throws Exception {
         input();
         process();
-        System.out.println(sb.toString());
     }
 }
