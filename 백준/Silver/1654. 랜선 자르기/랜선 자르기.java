a6a5e4d90b4f53ed3785d66a590293a79626800c
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-13 10:30:48 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17156 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -3,51 +3,59 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
     private static int K,N;
     private static int[] arr;
-    private static int max = Integer.MIN_VALUE;
+    private static int max = -1;
+    private static long answer;
 
-    public static void input() throws Exception{
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
         arr = new int[K];
-        for(int i = 0 ; i < arr.length; i++){
+
+        for(int i = 0 ; i < K ; i++){
             arr[i] = Integer.parseInt(br.readLine());
             max = Math.max(max,arr[i]);
         }
     }
 
+    //실행
     public static void process() {
         long s = 1;
         long e = max;
 
         while(s<=e){
+            int count = 0;
+
             long mid = (s+e)/2;
 
-            int cnt = 0;
-
-            for(int i = 0 ; i < arr.length; i++){
-                cnt+=arr[i]/mid;
+            for(int i = 0 ; i < K ; i++){
+                count+=arr[i]/mid;
             }
 
-            if(cnt>=N){
-                s = mid+1;
-            }else{
+            if(N>count){
                 e = mid-1;
+            }else{
+                answer = mid;
+                s = mid+1;
             }
+
         }
-        System.out.println(e);
+
+        System.out.println(answer);
     }
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
