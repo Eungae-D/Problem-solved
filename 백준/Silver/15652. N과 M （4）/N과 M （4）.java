6d1f37f574c526e97174bafebc756e80c7cefe22
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-14 17:39:03 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16356 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -3,46 +3,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
     private static int N,M;
     private static int[] arr;
 
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[M];
-
     }
-    public static void process(){
-        recur(0,0);
-    }
-
     public static void recur(int cur, int start){
-        if(cur ==M){
-            for(int i = 0 ; i < M; i++){
-                sb.append(arr[i]+" ");
+        if(cur == M){
+            for(int i = 0 ; i < M ; i++){
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = start ; i < N; i++){
-            arr[cur] = i+1;
+        for(int i = start ; i <= N ; i++){
+            arr[cur] = i;
             recur(cur+1,i);
         }
 
+
     }
 
-    public static void main(String[] args) throws Exception{
+    //실행
+    public static void process() {
+        recur(0,1);
+        System.out.println(sb);
+    }
+
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
-        System.out.println(sb);
     }
 }
