4188e168f9defbd8027443b1d0e8b47869123cca
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-16 23:46:31 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -4,58 +4,56 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
     private static int N,M;
-    private static int[] arr;
     private static int[] variables;
+    private static int[] arr;
 
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[M];
         variables = new int[N];
+        arr = new int[M];
 
         st = new StringTokenizer(br.readLine());
-
         for(int i = 0 ; i < N ; i++){
             variables[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(variables);
-
     }
-    public static void process(){
-        recur(0,0);
-    }
-
     public static void recur(int cur, int start){
         if(cur == M){
             for(int i = 0 ; i < M ; i++){
-                sb.append(arr[i]+" ");
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
 
-        for(int i = start ; i < N ; i++){
+        for(int i = start ; i < N; i++){
             arr[cur] = variables[i];
-            recur(cur+1,i+1);
+            recur(cur+1, i+1);
         }
+    }
 
+    //실행
+    public static void process() {
+        recur(0,0);
+        System.out.println(sb);
     }
 
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         input();
         process();
-        System.out.println(sb);
     }
 }
