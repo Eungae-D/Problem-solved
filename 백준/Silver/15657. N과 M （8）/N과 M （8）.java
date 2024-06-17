d67861b84cc69f3014df853d9bc321aa4f557ce8
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-17 15:29:50 +0900

    [Silver III] Title: N과 M (8), Time: 164 ms, Memory: 17968 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -4,56 +4,55 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static BufferedReader br;
     private static StringBuilder sb;
+    private static BufferedReader br;
     private static StringTokenizer st;
     private static int N,M;
     private static int[] arr;
     private static int[] variables;
 
-    public static void input() throws Exception{
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
-        N= Integer.parseInt(st.nextToken());
-        M= Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         arr = new int[M];
         variables = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
+        for(int i = 0 ; i < N ; i++){
             variables[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(variables);
     }
-
-    public static void process(){
-        recur(0,0);
-    }
-
     public static void recur(int cur, int start){
         if(cur == M){
-            for(int i = 0 ; i < M; i++){
-                sb.append(arr[i]+" ");
+            for(int i = 0 ; i < M ; i++){
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for(int i = start; i < N ; i++){
-            arr[cur]  = variables[i];
-            recur(cur+1, i);
+        for(int i = start ; i < N ; i++){
+            arr[cur] = variables[i];
+            recur(cur+1,i);
         }
-
-
     }
 
-    public static void main(String[] args) throws Exception{
+    //실행
+    public static void process() {
+        recur(0,0);
+        System.out.println(sb);
+    }
+
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
-        System.out.println(sb);
     }
 }
