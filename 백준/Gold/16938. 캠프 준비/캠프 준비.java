70fbd2b4b11980b56f98b64169dbef1a0c42f726
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-18 02:14:04 +0900

    [Gold V] Title: 캠프 준비, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -7,52 +7,49 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N,L,R,X;
-    private static int[]problems;
-    private static int answer=0;
+    private static int[] variables;
+    private static int count = 0;
 
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
 
-        N =Integer.parseInt(st.nextToken());
-        L =Integer.parseInt(st.nextToken());
-        R =Integer.parseInt(st.nextToken());
-        X =Integer.parseInt(st.nextToken());
-
-        problems = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N; i++){
-            problems[i] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        variables = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
+            variables[i] = Integer.parseInt(st.nextToken());
         }
 
-
     }
-
-    public static void process() {
-        recur(0,0,0,Integer.MAX_VALUE,Integer.MIN_VALUE);
-    }
-
-    public static void recur(int cur,int cal,int cnt, int min, int max){
-        if (cur == N) {
-            if (cnt >= 2 && cal >= L && cal <= R && max - min >= X) {
-                answer++;
+    public static void recur(int cur, int sum,int cnt, int min, int max){
+        if(cur == N){
+            if(cnt>=2 && sum>=L && sum<=R && max-min >= X){
+                count++;
             }
             return;
         }
 
-        // 현재 문제를 포함하는 경우
-        recur(cur + 1, cal + problems[cur], cnt + 1, Math.min(min, problems[cur]), Math.max(max, problems[cur]));
-
-        // 현재 문제를 포함하지 않는 경우
-        recur(cur + 1, cal, cnt, min, max);
+        recur(cur+1,sum+variables[cur],cnt+1,Math.min(min,variables[cur]),Math.max(max,variables[cur]));
+        recur(cur+1,sum,cnt,min,max);
     }
 
+    //실행
+    public static void process() {
+        recur(0,0,0,Integer.MAX_VALUE,Integer.MIN_VALUE);
+        System.out.println(count);
+    }
 
 
     public static void main(String[] args) throws Exception {
         input();
         process();
-        System.out.println(answer);
     }
 }
