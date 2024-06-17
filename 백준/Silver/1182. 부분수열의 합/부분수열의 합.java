1e2659d114586767f9218246cfee776188640614
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-18 00:45:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,55 +1,51 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
     private static int N,S;
-    private static int[] variables;
+    private static int[] arr;
+    private static int answer;
 
-
-    private static int count;
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
 
-        variables = new int[N];
+        arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < N ; i++){
-            variables[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
     }
-    public static void process(){
-        recur(0,0);
-        if(S==0){
-            count-=1;
-        }
-        System.out.println(count);
-    }
-
-    public static void recur(int cur, int sum){
+    public static void recur(int cur, int sum, int cnt){
         if(cur == N){
-            if(sum==S){
-                count++;
+            if(sum == S && cnt!=0){
+                answer++;
             }
             return;
         }
-        recur(cur+1,sum+variables[cur]);
-        recur(cur+1,sum);
+
+        recur(cur+1, sum+arr[cur],cnt+1);
+        recur(cur+1,sum,cnt);
     }
 
-    public static void main(String[] args) throws Exception{
+    //실행
+    public static void process() {
+        recur(0,0,0);
+        System.out.println(answer);
+    }
+
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
