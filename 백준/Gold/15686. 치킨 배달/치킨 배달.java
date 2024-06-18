9f7850652b93e8f8780a9117da6682da3c7e168a
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-18 15:14:54 +0900

    [Gold V] Title: 치킨 배달, Time: 240 ms, Memory: 17532 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -4,60 +4,51 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static class Location{
+        int x;
+        int y;
+        Location(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
     private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N,M;
-    private static int[][]arr;
+    private static int[][] arr;
     private static int min = Integer.MAX_VALUE;
     private static ArrayList<Location> chicken = new ArrayList<>();
     private static ArrayList<Location> house = new ArrayList<>();
     private static boolean[] visited;
 
-    public static class Location{
-        int x;
-        int y;
-        public Location(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
 
-        N= Integer.parseInt(st.nextToken());
-        M= Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         arr = new int[N][N];
-        for(int i = 0 ; i < N; i++){
+        for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0 ; j < N ; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
                 if(arr[i][j]==1){
                     house.add(new Location(i,j));
-                } else if (arr[i][j]==2) {
+                }else if(arr[i][j]==2){
                     chicken.add(new Location(i,j));
                 }
             }
         }
-
         visited = new boolean[chicken.size()];
-
-
     }
-
-    public static void process() {
-        recur(0,0);
-        System.out.println(min);
-    }
-
     public static void recur(int cur, int start){
-        if(cur == M){
+        if(cur==M){
             int ans = 0;
-            for(int i = 0 ; i < house.size();i++){
+            for(int i = 0 ; i < house.size(); i++){
                 int temp = Integer.MAX_VALUE;
                 for(int j = 0 ; j < chicken.size(); j++){
                     if(visited[j]){
@@ -67,17 +58,22 @@ public class Main {
                 }
                 ans+=temp;
             }
-            min = Math.min(ans,min);
+            min = Math.min(min,ans);
             return;
         }
 
-        for(int i = start; i<chicken.size(); i++){
+        for(int i = start; i < chicken.size(); i++){
             visited[i] = true;
             recur(cur+1,i+1);
             visited[i] = false;
         }
     }
 
+    //실행
+    public static void process() {
+        recur(0,0);
+        System.out.println(min);
+    }
 
 
     public static void main(String[] args) throws Exception {
