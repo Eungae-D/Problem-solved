44477003da94fa9c5561c0a67ebd9dce5f434c62
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-20 15:52:13 +0900

    [Gold IV] Title: 스도쿠, Time: 400 ms, Memory: 22412 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -6,9 +6,9 @@ public class Main {
     private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int[][] arr = new int[9][9];
+    private static int[][]arr = new int[9][9];
 
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
@@ -21,19 +21,20 @@ public class Main {
         }
     }
     public static boolean check(int row, int col, int num){
-        //0, 0 , 1
+        //0 , 0 , 1
+        //row
         for(int i = 0 ; i < 9 ; i++){
             if(arr[row][i] == num){
                 return false;
             }
         }
-
+        //col
         for(int i = 0 ; i < 9 ; i++){
-            if(arr[i][col] ==num){
+            if(arr[i][col] == num){
                 return false;
             }
         }
-
+        //3 *3
         int startRow = (row/3)*3;
         int startCol = (col/3)*3;
         for(int i = 0 ; i < 3 ; i++){
@@ -47,40 +48,40 @@ public class Main {
     }
 
     public static boolean recur(int cur){
-        if(cur == 81){
-            return true;
-        }
+        if(cur == 81) return true;
 
-        int row = cur / 9;
-        int col = cur % 9;
+        int row = cur/9;
+        int col = cur%9;
 
-        if(arr[row][col] != 0){
+        if(arr[row][col]!=0){
             return recur(cur+1);
         }
 
-        for(int i = 1 ; i <= 9; i++){
-           if(check(row,col,i)){
-               arr[row][col] = i;
-               if(recur(cur+1)){
-                  return true;
-               }
-               arr[row][col] = 0;
-           }
+        for(int i = 1 ; i <= 9 ; i++){
+            if(check(row,col,i)){
+                arr[row][col] = i;
+                if(recur(cur+1)){
+                    return true;
+                }
+                arr[row][col] = 0;
+            }
         }
         return false;
     }
 
+    //실행
     public static void process() {
         if(recur(0)){
-           for(int i = 0 ; i < 9 ; i++){
-               for(int j = 0 ; j < 9 ; j++){
-                   sb.append(arr[i][j]).append(" ");
-               }
-               sb.append("\n");
-           }
+            for(int i = 0 ; i < 9 ; i++){
+                for(int j = 0 ; j < 9 ; j++){
+                    sb.append(arr[i][j]+" ");
+                }
+                sb.append("\n");
+            }
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
+
 
     public static void main(String[] args) throws Exception {
         input();
