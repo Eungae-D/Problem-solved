100c136bc4d0eae3f8c6452f58fb9ec1b3a511fb
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-20 13:48:17 +0900

    [Gold V] Title: 암호 만들기, Time: 108 ms, Memory: 15008 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -7,59 +7,61 @@ public class Main {
     private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int L, C;
-    private static char[] characters, password;
+    private static int L,C;
+    private static char[]arr,password;
 
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
 
+        st = new StringTokenizer(br.readLine());
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        characters = new char[C];
+
+        arr = new char[C];
         password = new char[L];
 
-
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < C; i++) {
-            characters[i] = st.nextToken().charAt(0);
+        for(int i = 0 ; i < C ; i++){
+            arr[i] = st.nextToken().charAt(0);
         }
-
-        Arrays.sort(characters);
+        Arrays.sort(arr);
+    }
+    public static int[] addCnt(char x){
+        if(x=='a' || x =='e' || x == 'i' || x == 'o' || x == 'u'){
+            return new int[] {0,1};
+        }else{
+            return new int[] {1, 0};
+        }
     }
 
-    public static void process() {
-        recur(0, 0);
-        System.out.println(sb);
-    }
-
-    public static void recur(int cur,int start) {
-        if(cur == L) {
-            if(isValid(password)) {
-                for(char c : password) sb.append(c);
-                sb.append('\n');
+    public static void recur(int cur, int cnt, int a, int b){
+        if(cnt == L){
+            if(a>=2 && b >=1){
+                sb.append(password).append("\n");
             }
             return;
         }
 
-        for(int i = start; i < C; i++) {
-            password[cur] = characters[i];
-            recur(cur + 1, i + 1);
-        }
-    }
-    
-    private static boolean isValid(char[] arr) {
-        int v = 0; // 모음
-        int c = 0; // 자음
-
-        for(char temp : arr) {
-            if(temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u') v++;
-            else c++;
+        if(cur == C){
+            return;
         }
 
-        return v >= 1 && c >= 2;
+        int[] count = addCnt(arr[cur]);
+
+        password[cnt] = arr[cur];
+        recur(cur+1,cnt+1,a+count[0],b+count[1]);
+        recur(cur+1,cnt,a,b);
+
     }
+
+    //실행
+    public static void process() {
+        recur(0,0,0,0);
+        System.out.println(sb);
+    }
+
 
     public static void main(String[] args) throws Exception {
         input();
