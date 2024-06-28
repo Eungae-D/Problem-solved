279bfe81251f816be3cb8ab3f18f73d4d41c41e4
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-06-28 21:41:06 +0900

    [Gold IV] Title: 출근 경로, Time: 116 ms, Memory: 16676 KB -BaekjoonHub

--- 백준/Gold/5569. 출근 경로/출근 경로.java
+++ 백준/Gold/5569. 출근 경로/출근 경로.java
@@ -3,45 +3,77 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int W,H;
+    private static int N;
+    private static int M;
     private static int MOD = 100000;
-    private static int [][][][]dp;
+    private static int[][] arr;
+    private static long[][][][] dp;
 
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
 
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        dp = new int[W+1][H+1][2][2];
+        M = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i <= W ; i++){
-            dp[i][1][0][0]= 1;
-        }
-        for(int i = 1; i <= H ; i++){
-            dp[1][i][1][0]= 1;
-        }
-    }
+        arr = new int[N+10][M+10];
 
-    public static void process() {
-        for(int i = 2; i <=W ; i++){
-            for(int j = 2; j <=H; j++){
-                dp[i][j][0][0] = (dp[i - 1][j][0][0] + dp[i - 1][j][0][1]) % MOD;
-                dp[i][j][0][1] = dp[i - 1][j][1][0] % MOD;
-                dp[i][j][1][0] = (dp[i][j-1][1][1] + dp[i][j-1][1][0]) % MOD;
-                dp[i][j][1][1] = dp[i][j - 1][0][0] % MOD;
+        dp = new long[N+10][M+10][3][3];
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=M; j++){
+                for(int k=0; k<3; k++){
+                    for(int l=0; l<3; l++){
+                        dp[i][j][k][l] = -1;
+                    }
+                }
             }
         }
+    }
+    public static boolean check(int x, int y, int dir, int preDir){
+        if(x>N || y>M) return true;
+
+        if(x-1>=1 && y-1>=1){
+            if(dir == preDir) return true;
+        }
+        return false;
+    }
+    public static long recur(int x, int y, int dir, int preDir){
+
+        if (check(x,y,dir,preDir)) return 0;
+
+        if (x == N && y == M){ return 1;}
+        // dp 조건
+        if (dp[x][y][dir][preDir] != -1) return dp[x][y][dir][preDir];
+
+        arr[x][y] = dir;
+        //남쪽방향
+        long south = recur(x+1,y,2,arr[x][y-1]);
+        //동쪽방향
+        long east = recur(x,y+1,1,arr[x-1][y]);
+
+        arr[x][y] = 0;
+
+        long result = (south+east)%MOD;
+        dp[x][y][dir][preDir] = result;
+        return dp[x][y][dir][preDir];
+    }
+
+    //실행
+    public static void process() {
+        recur(1,1,0,0);
+        System.out.println(dp[1][1][0][0]);
     }
 
 
     public static void main(String[] args) throws Exception {
         input();
         process();
-        System.out.println((dp[W][H][0][0]+dp[W][H][0][1]+dp[W][H][1][0]+dp[W][H][1][1]) % MOD);
     }
 }
