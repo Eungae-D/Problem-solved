5268c7cfdab0afd88ec5f8f43d1a0df28a2584a3
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-03 19:35:49 +0900

    [Gold IV] Title: 공룡게임, Time: 104 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Gold/20544. 공룡게임/공룡게임.java
+++ 백준/Gold/20544. 공룡게임/공룡게임.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,45 +7,49 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N;
-    private static int[][][][] dp;
+    private static long[][][][] dp;
 
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
 
-        dp = new int[N][3][2][2];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 2; k++) {
-                    for (int l = 0; l < 2; l++) {
-                        dp[i][j][k][l] = -1;
+        dp = new long[N+1][4][3][2];
+
+        for(int i = 0 ; i < N+1; i++){
+            for(int j = 0 ; j < 4 ; j++){
+                for(int k = 0 ; k < 3 ; k++){
+                    for(int l = 0 ; l < 2 ; l++){
+                        if(i==N && l==1){
+                            if(j > 2 || k >= 2) continue;
+                            dp[i][j][k][l] = 1;
+                        }
                     }
                 }
             }
         }
     }
 
-    static long recur(int cur, int cnt1, int cnt2, int two) {
-        if (cnt1 > 2 || cnt2 >= 2) return 0;
-        if (cur == N) return two;
-
-        if (dp[cur][cnt1][cnt2][two] != -1) return dp[cur][cnt1][cnt2][two];
-
-        dp[cur][cnt1][cnt2][two] = (int)((recur(cur + 1, 0, 0, two) // 바닥
-                + recur(cur + 1, cnt1 + 1, cnt2, two) // 높이 1인 선인장
-                + recur(cur + 1, cnt1 + 1, cnt2 + 1, 1)) % 1000000007); // 높이 2인 선인장
-        return dp[cur][cnt1][cnt2][two];
-    }
-
+    //실행
     public static void process() {
-        long result = recur(1, 0, 0, 0);
-        System.out.println(result);
+        for(int i = N-1 ; i>= 1 ; i--){
+            for(int j = 2 ; j >= 0 ; j--){
+                for(int k = 1 ; k >= 0 ; k--){
+                    for(int l = 1 ; l >= 0 ; l--){
+                        dp[i][j][k][l] = (dp[i+1][0][0][l] + dp[i+1][j+1][0][l]+dp[i+1][j+1][k+1][1]) % 1000000007;
+                    }
+                }
+            }
+        }
+        System.out.println(dp[1][0][0][0]);
     }
+
 
     public static void main(String[] args) throws Exception {
         input();
         process();
+
     }
 }
