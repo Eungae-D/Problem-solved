00c453f0c85e1d6bc3a6e9274df3ba20ed9e45f0
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-04 20:18:56 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -7,40 +7,35 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N;
-    private static int[]dp;
+    private static int[] dp;
 
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-
+        N = Integer.parseInt(br.readLine());
         dp = new int[N+1];
+    }
 
+    //실행
+    public static void process() {
         if(N == 1){
             System.out.println(1);
             return;
         }
+        
         dp[1] = 1;
         dp[2] = 2;
-
-    }
-
-    public static void process() {
-        for(int i = 3; i <=N; i++){
-            dp[i] = (dp[i-1]+dp[i-2])%10007;
+        for(int i = 3; i <= N ; i++){
+            dp[i] = (dp[i-2]+ dp[i-1])%10007;
         }
+        System.out.println(dp[N]);
     }
 
 
     public static void main(String[] args) throws Exception {
         input();
-        if(N==1){
-            return;
-        }
         process();
-        System.out.println(dp[N]);
     }
 }
