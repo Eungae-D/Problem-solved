83f43489e9a2e8e0676f9bb4336e10c98781f8d6
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-11 15:15:33 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 108 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -7,36 +7,34 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N;
-    private static int[]arr;
+    private static int[] arr;
     private static int answer = 0;
 
-
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
-
-        for(int i = 0 ; i < N; i++){
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
     }
 
+    //실행
     public static void process() {
-        if( N == 1){
+        if( N == 1 ){
             System.out.println(0);
-        }else{
-            for(int i = N-1; i > 0; i--){
-                while (arr[i]<=arr[i-1]){
+        }else {
+            for(int i = N-1 ; i > 0 ; i--){
+                while(arr[i-1]>=arr[i]){
                     answer++;
-                    arr[i-1] = arr[i-1] -1;
+                    arr[i-1] -= 1;
                 }
             }
             System.out.println(answer);
         }
-
     }
 
 
