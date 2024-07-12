d8adfb83cfc47b3079ebec3d9851d4c637a67259
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-12 16:25:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 188 ms, Memory: 24960 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -5,31 +5,30 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
     private static int N,K;
-    private static Queue<Integer> que;
-    private static int answer;
     private static int d = 0;
+    private static Queue<Integer> que = new LinkedList<>();
     private static boolean[] visited = new boolean[200010];
     private static int[] mul = {1,1,2};
     private static int[] add = {-1,1,0};
 
-    public static void input() throws Exception{
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
-        que = new LinkedList<>();
     }
     public static void bfs(){
-        while (!que.isEmpty()){
-            int sz = que.size();
+        while(!que.isEmpty()){
+            int size = que.size();
 
-            for(int s = 0 ; s < sz ; s++){
+            for(int s = 0 ; s < size ; s++){
                 int cur = que.poll();
 
                 if(cur == K){
@@ -38,9 +37,9 @@ public class Main {
                 }
 
                 for(int i = 0 ; i < 3 ; i++){
-                    int nxt = mul[i] * cur + add[i];
+                    int nxt = mul[i]*cur + add[i];
 
-                    if(nxt < 0 || nxt > 200000 || visited[nxt]) continue;
+                    if(nxt<0 || nxt > 200000 || visited[nxt]) continue;
 
                     que.add(nxt);
                     visited[nxt] = true;
@@ -51,17 +50,19 @@ public class Main {
         }
     }
 
+    //실행
     public static void process() {
         if(N==K){
             System.out.println(d);
-            return;
         }else{
             que.add(N);
-            visited[N] = true;
+            visited[N]= true;
             bfs();
         }
     }
-    public static void main(String[] args) throws Exception{
+
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
