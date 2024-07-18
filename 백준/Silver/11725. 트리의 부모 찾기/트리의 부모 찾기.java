76b1c9c57f04f988a7796345ec7f92efc21cb000
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-18 19:52:43 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 696 ms, Memory: 73508 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -5,27 +5,27 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static StringBuilder sb;
     private static int N;
-    private static List<List<Integer>> v;
-    private static int[]par;
+    private static ArrayList<ArrayList<Integer>> v;
+    private static int[] parents;
 
-    public static void input () throws Exception {
+    //입력
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
 
         v = new ArrayList<>();
-        par = new int[N+1];
+        parents = new int[N+1];
 
-        for(int i = 0 ; i <= N; i++){
+        for(int i = 0 ; i<= N ; i++){
             v.add(new ArrayList<>());
         }
-
-        for(int i = 0 ; i < N-1; i++){
+        for(int i = 0 ; i < N-1 ; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
@@ -33,23 +33,27 @@ public class Main {
             v.get(s).add(e);
             v.get(e).add(s);
         }
-
     }
     public static void dfs(int cur, int prv){
         for(int nxt : v.get(cur)){
             if(nxt == prv) continue;
 
-            par[nxt] = cur;
-            dfs(nxt,cur);
+            parents[nxt] = cur;
+            dfs(nxt, cur);
         }
     }
-    public static void process(){
+
+    //실행
+    public static void process() {
         dfs(1,0);
-        for(int i = 2; i <= N; i++){
-            System.out.println(par[i]);
+        for(int i = 2 ; i <= N ; i++){
+            sb.append(parents[i]).append("\n");
         }
+        System.out.println(sb);
     }
-    public static void main(String[] args) throws Exception{
+
+
+    public static void main(String[] args) throws Exception {
         input();
         process();
     }
