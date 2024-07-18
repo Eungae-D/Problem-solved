6caf920824c91437d45e2b6c8c452391027cd460
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-18 20:43:32 +0900

    [Gold V] Title: 트리와 쿼리, Time: 672 ms, Memory: 87400 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,29 +8,27 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N,R,Q;
-    private static List<List<Integer>> v;
-    private static int[]par;
-    private static int[]size;
+    private static ArrayList<ArrayList<Integer>> v;
+    private static int[] size;
 
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-
         st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
 
         v = new ArrayList<>();
-        par = new int[N+1];
-        size = new int[N+1];
-
-        for(int i = 0 ; i <=N; i++){
+        for(int i = 0 ; i <= N ; i++){
             v.add(new ArrayList<>());
         }
 
-        for(int i = 0 ; i < N-1; i++){
+        size = new int[N+1];
+
+        for(int i = 0 ; i < N-1 ; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
@@ -39,28 +36,24 @@ public class Main {
             v.get(s).add(e);
             v.get(e).add(s);
         }
-
         process();
-
-        for(int i = 0 ; i < Q; i++){
-            System.out.println(size[Integer.parseInt(br.readLine())]);
+        for(int i = 0 ; i < Q ; i++){
+            sb.append(size[Integer.parseInt(br.readLine())]).append("\n");
         }
+        System.out.println(sb);
     }
-
-
     public static void dfs(int cur, int prv){
         size[cur] = 1;
+
         for(int nxt : v.get(cur)){
             if(nxt == prv) continue;
 
-            par[nxt] = cur;
-
-            dfs(nxt, cur);
-
-            size[cur] += size[nxt];
+            dfs(nxt,cur);
+            size[cur]+= size[nxt];
         }
     }
 
+    //실행
     public static void process() {
         dfs(R,0);
     }
