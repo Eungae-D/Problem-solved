ea8e523eb939f259f30f28bb9288e9fcccade5af
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-07-27 16:39:01 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 4.88 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2 ; i <= 1000000 ; i++){
+            if(n%i==1){
+                answer = i;
+                // System.out.print(answer);
+                break;
+            }
+            
+        }
+        return answer;
+    }
+}
