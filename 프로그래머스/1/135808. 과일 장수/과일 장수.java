6fb015ac1234228d83b988a52763b1678fa276e3
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-08-01 15:47:01 +0900

    [level 1] Title: 과일 장수, Time: 59.56 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        Arrays.sort(score);
+        
+        for(int i = score.length-1; i >= m-1 ; i-=m){
+            answer += score[i-m+1] * m;
+        }
+        
+        
+        
+        return answer;
+    }
+}
