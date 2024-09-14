a43d8bdaaf0866a775abe92d4fa65880ab917705
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-09-15 01:57:17 +0900

    [Silver I] Title: 보석 도둑, Time: 124 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.java
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -1,37 +1,35 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     private static StringBuilder sb;
     private static BufferedReader br;
-    private static StringTokenizer st;
-    private static long K;
+//    private static StringTokenizer st;
+    private static long N;
     private static int count = 0;
+
 
     //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
+//        st = new StringTokenizer(br.readLine());
 
-        K = Long.parseLong(br.readLine());
+        N = Long.parseLong(br.readLine());
     }
 
     //실행
     public static void process() {
-        long tmp = K;
 
-        //소인수분해 구하기
-        for(long i = 2 ; i*i < K+1 ; i++){
-            while(tmp % i == 0){
+        for(long i = 2 ; i*i <= N; i++){
+            while(N%i==0){
                 sb.append(i+" ");
                 count++;
-                tmp /= i;
+                N/=i;
             }
         }
-
-        if(tmp != 1){
-            sb.append(tmp);
+        if(N!=1){
+            sb.append(N);
             count++;
         }
 
