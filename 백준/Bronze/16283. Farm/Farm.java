87dc6f87c3879087b24957e366996843ee9efff1
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-09-14 18:04:30 +0900

    [Bronze II] Title: Farm, Time: 140 ms, Memory: 16636 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.java
+++ 백준/Bronze/16283. Farm/Farm.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     private static StringBuilder sb;
@@ -8,12 +7,12 @@ public class Main {
     private static StringTokenizer st;
     private static int A,B,N,W;
 
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
-
         st = new StringTokenizer(br.readLine());
+
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
@@ -21,23 +20,22 @@ public class Main {
 
     }
 
+    //실행
     public static void process() {
         int count = 0;
-
-        for(int i = 1; i <= 1000 ; i++) {
-            for(int j = 1 ; j <= 1000 ; j++){
-
-                if(i+j == N && i*A + j*B == W){
-                    count++;
+        for(int i = 1 ; i <= 1000; i++){
+            for(int j = 1 ; j <= 1000; j++){
+                if(i+j == N && A*i + B*j == W){
                     sb.append(i+" "+j);
+                    count++;
                 }
             }
         }
 
-        if(count != 1){
+        if(count!=1){
             System.out.println(-1);
         }else{
-            System.out.println(sb);
+            System.out.println(sb.toString());
         }
     }
 
