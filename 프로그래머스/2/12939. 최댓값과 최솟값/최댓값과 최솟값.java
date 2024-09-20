4b681c4d651f3e9a22fc093032b4b6c4da52673d
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-09-21 00:32:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.95 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static String[] arr;
+    private static String answer = "";
+    private static int max = Integer.MIN_VALUE;
+    private static int min = Integer.MAX_VALUE;
+    
+    public String solution(String s) {
+    
+        
+        arr = s.split(" ");
+        for(int i = 0 ; i < arr.length; i++){
+            int temp = Integer.valueOf(arr[i]);
+            max = Math.max(max, temp);
+            min = Math.min(min, temp);
+        }
+        
+        return min+" "+max;
+    }
+}
