5828e592c950a4ff1eb92b65767457b571af4454
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-10-01 21:09:51 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.07 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        // Integer[] B_Integer = Arrays.stream(B).boxed().toArray(Integer[]::new);
+        // Arrays.sort(B_Integer, Collections.reverseOrder());
+
+        for(int i = 0 ; i < A.length; i++){
+            answer+= A[i]*B[A.length-i-1];
+        }
+        return answer;
+    }
+}
