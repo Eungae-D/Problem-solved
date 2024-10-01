18eb420695e49ea69926b84818589ab76f66a6c3
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-10-01 20:43:27 +0900

    [level 2] Title: 올바른 괄호, Time: 15.35 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static String[]arr;
+    private static int size = 0;
+    private static Stack<Character> stack = new Stack<>();
+    
+    boolean solution(String s) {
+        
+        size = s.length();
+        
+        for(int i = 0 ; i < size ; i++){
+           char temp = s.charAt(i);
+            if(temp == '('){
+                stack.push(temp);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        
+        if(stack.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
