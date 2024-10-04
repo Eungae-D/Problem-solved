b75798897aa3db82e68f0fabe16b9d9460dda4d7
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-10-04 10:33:41 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int binary(int n){
+        int one = 0; 
+
+        while (n > 0) {
+            if(n % 2 == 1){
+                one++;
+            }
+            n /= 2;
+        }
+        return one;
+    }
+    
+    public int solution(int n) {
+        
+        int answer = 0;
+        int oneCnt = binary(n);
+        
+        while(true){
+            int nextCnt = binary(++n);
+            if(oneCnt==nextCnt) break;
+        }
+        
+        answer = n;
+
+        return answer;
+    }
+}
