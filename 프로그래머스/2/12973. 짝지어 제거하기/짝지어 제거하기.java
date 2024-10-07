34a6f64ebf6589c6896302ce5f676626a9d0d58d
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-10-07 11:40:31 +0900

    [level 2] Title: 짝지어 제거하기, Time: 74.09 ms, Memory: 63 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Solution{
+    private static Stack<Character> stack = new Stack<>();
+    private static char[] ch;
+    public int solution(String s){
+        ch = s.toCharArray();
+        
+        for(char c : ch){
+            if(stack.isEmpty()){
+                stack.push(c);
+            }else{
+                if(stack.peek()==c){
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            }
+        }
+    
+        
+    
+
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
