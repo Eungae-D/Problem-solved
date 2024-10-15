8628be46f3e2ff36c80e7d8562416a4ade594cc0
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-10-15 13:28:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 63.51 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private String shift(String s){
+        return s.substring(1)+s.charAt(0);
+    }
+    
+    private boolean verification(String s){
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0 ; i < s.length(); i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(stack.peek()=='(' && s.charAt(i)==')'){
+                    stack.pop();
+                }else if(stack.peek()=='[' && s.charAt(i)==']'){
+                    stack.pop();
+                }else if(stack.peek()=='{' && s.charAt(i)=='}'){
+                    stack.pop();
+                }else return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    public int solution(String s) {
+        
+        int answer = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(verification(s)){
+                answer++;
+            }
+            
+            s = shift(s);
+        }
+        
+        return answer;
+    }
+}
