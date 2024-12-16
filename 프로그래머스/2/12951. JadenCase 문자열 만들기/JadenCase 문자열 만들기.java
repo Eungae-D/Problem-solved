535e3088ba8434c38351fcaf639ae3cfb35fe292
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-12-16 16:26:55 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 4.98 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -7,32 +7,23 @@ class Solution {
         String answer = "";
         String[] arr = s.split(" ");
         
- 
-        for(int i=0; i<arr.length; i++) {
+        for(int i = 0 ; i < arr.length ; i++){
             String now = arr[i];
- 
-            if(arr[i].length() == 0) {
-                answer += " "; // 문자가 없을 경우 공백
-            } 
-            // 문자가 있다면
-            else {
             
-                answer += now.substring(0, 1).toUpperCase();
-                answer += now.substring(1, now.length()).toLowerCase();
+            //문자 없으면 공백
+            if(arr[i].length() == 0){
+                answer += " "; 
+            //문자 있으면
+            }else{
+                answer += now.substring(0,1).toUpperCase();
+                answer += now.substring(1,now.length()).toLowerCase();
                 answer += " ";
             }
-            
         }
-
-        System.out.println(answer);
-        
-         // 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
-        if(s.substring(s.length()-1, s.length()).equals(" ")){
+        if(s.substring(s.length()-1,s.length()).equals(" ")){
             return answer;
         }
         
-        
-        // 맨 마지막 " " 제거 후 반환
-        return answer.substring(0, answer.length()-1);
+        return answer.substring(0,answer.length()-1);
     }
 }
