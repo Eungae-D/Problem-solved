b59bca0bb17e9dbbc1a9a9839c9ab13ae079eab1
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-12-17 11:56:49 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -4,32 +4,32 @@ import java.util.*;
 class Solution {
     private static int gcd(int a, int b){
         int temp = 0;
+        
         while(a%b!=0){
             temp = a%b;
             a = b;
             b = temp;
         }
+        
         return b;
     }
-    public int solution(int[] arr) {
+    
+    public int solution(int[] arr){
         int answer = 0;
-
-        if(arr.length == 1) return arr[0];
+        
+        if(arr.length == 1){
+            return arr[0];
+        }
         
         int gcd = gcd(arr[0],arr[1]);
         answer = (arr[0]*arr[1])/gcd;
         
-        System.out.println(answer);
-        
-        
         if(arr.length>2){
-            for(int i = 2 ; i < arr.length; i++){
+            for(int i = 2 ; i < arr.length ; i++){
                 gcd = gcd(answer,arr[i]);
                 answer = (answer*arr[i])/gcd;
             }
         }
-
-            
         return answer;
     }
 }
