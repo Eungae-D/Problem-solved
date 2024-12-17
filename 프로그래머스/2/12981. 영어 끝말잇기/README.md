8384229ff06445ec7ef0f08340653128aa92f164
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-12-17 12:46:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.22 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.27 ms
+메모리: 73 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 14일 16:23:53
+2024년 12월 17일 12:46:17
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -8,21 +8,20 @@ class Solution {
         }
         return true;
     }
-    public int[] solution(int n, String[] words) {
+    public int[] solution(int n, String[] words){
         int[] answer = new int[2];
         
         ArrayList<String> list = new ArrayList<>();
         list.add(words[0]);
         
         for(int i = 1 ; i < words.length ; i++){
-            if(list.contains(words[i]) || same(list.get(i-1),words[i])){
-                answer[0] = i%n+1;
-                answer[1] = i/n+1;
+            if(list.contains(words[i])||same(list.get(i-1),words[i])){
+                answer[0] = i%n +1;
+                answer[1] = i/n +1;
                 break;
             }
             list.add(words[i]);
         }
         return answer;
-
     }
 }
