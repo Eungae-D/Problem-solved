81fc2c9ad5fa172c322980fe7f00d54f05076c85
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2024-12-23 22:21:42 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -4,7 +4,6 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
         for(int i = 1 ; i <= n ; i++){
             if(n%i==0){
                 answer+=i;
