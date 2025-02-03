88726d0074045dcdc39976790d6a43f3afff0567
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-02-03 21:48:46 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -3,29 +3,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int[] arr;
+    private static StringBuilder sb;
+    private static int[] arr = new int[10];
     private static int sum = 0;
-    private static int height,nextHeight,heightABS,nextHeightABS;
+    private static int height,nextHeight;
 
-    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
-        arr = new int[10];
-        for(int i = 0 ; i < 10 ; i++){
+        for(int i = 0 ; i < 10; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
+
     }
 
-    //실행
-    public static void process() {
-        for(int i = 0 ; i < 10 ; i++){
-            sum += arr[i];
-            if(sum<=100){
+    public static void process(){
+        for(int i = 0 ; i < 10; i++){
+            sum+=arr[i];
+            if(sum <= 100){
                 height = sum;
             }else{
                 nextHeight = sum;
@@ -33,20 +31,13 @@ public class Main {
             }
         }
 
-        heightABS = Math.abs(100-height);
-        nextHeightABS = Math.abs(100 - nextHeight);
 
-        if(heightABS > nextHeightABS){
-            System.out.println(nextHeight);
-        }else if(heightABS < nextHeightABS){
-            System.out.println(height);
-        }else{
-            System.out.println(nextHeight);
-        }
+        if(Math.abs(100-height)== Math.abs(100-nextHeight)) System.out.println(nextHeight);
+        else if(Math.abs(100-height) < Math.abs(100-nextHeight)) System.out.println(height);
+        else System.out.println(nextHeight);
     }
 
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         input();
         process();
     }
