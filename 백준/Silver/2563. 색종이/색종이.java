4ae767e0e58b62f0dd8669847a7fb43ec0cec896
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-02-04 22:37:26 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -3,41 +3,45 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb;
     private static BufferedReader br;
+    private static StringBuilder sb;
     private static StringTokenizer st;
     private static int N;
     private static int[][]arr = new int[110][110];
-    private static int answer = 0;
+    private static int answer;
 
-    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
-
+        N = Integer.parseInt(st.nextToken());
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            process(x,y);
-        }
-    }
 
-    //실행
-    public static void process(int x, int y) {
-        for(int i = x ; i < x+10 ; i++){
-            for(int j = y ; j < y+10 ; j++){
-                if(arr[i][j] == 0 ){
-                    arr[i][j] = 1;
+            process(x,y,x+10,y+10);
+        }
+
+        for(int i = 0 ; i < arr.length; i++){
+            for(int j = 0 ; j < arr[i].length ; j++){
+                if(arr[i][j]!=0){
                     answer++;
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void process(int x, int y, int i, int j){
+        for(int x1 = x ; x1 < i ; x1++){
+            for(int y1 = y ; y1 < j ; y1++){
+                arr[x1][y1]++;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
         input();
         System.out.println(answer);
     }
