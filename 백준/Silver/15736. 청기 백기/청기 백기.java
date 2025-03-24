f50220c49d9135f55391145458e7e3ebd6f5f9d9
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-03-24 20:51:03 +0900

    [Silver IV] Title: 청기 백기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/15736. 청기 백기/청기 백기.java
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -1,27 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     private static BufferedReader br;
-    private static long N;
-    private static int ans = 1;
+    private static StringBuilder sb;
+    private static StringTokenizer st;
+    private static int N;
+    private static int answer = 1;
 
-    public static void input() throws Exception{
+    public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
     }
 
-    public static void process() {
-        for(int i = 2 ; i * i <= N ; i++){
-            ans++;
+    public static void process(){
+        for(int i = 2 ; i*i <= N ; i++){
+            answer++;
         }
-
-        System.out.println(ans);
+        System.out.println(answer);
     }
-
 
     public static void main(String[] args) throws Exception{
         input();
