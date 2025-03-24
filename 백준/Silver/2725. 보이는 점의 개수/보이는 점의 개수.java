0a9587bc7da9e0b576eb007cc989197342e11c26
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-03-24 22:16:13 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 152 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.java
@@ -3,55 +3,55 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb;
     private static BufferedReader br;
+    private static StringBuilder sb;
     private static StringTokenizer st;
     private static int T,N;
     private static int[] arr = new int[1010];
 
-    //최대공약수
-    public static int gcd(int i, int j){
-        while(i%j != 0){
-            int temp = i%j;
-            i = j;
-            j = temp;
-        }
-        return j;
-    }
-
-    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
         sb = new StringBuilder();
 
-        T = Integer.parseInt(br.readLine());
+        process();
 
+        T = Integer.parseInt(st.nextToken());
+        for(int tc = 0 ; tc<T ; tc++){
+            N = Integer.parseInt(br.readLine());
+            sum(N);
+        }
+    }
+    public static void sum(int n){
+        sb.append(arr[n]).append("\n");
+    }
+    public static int gcd(int a, int b){
+        while (a%b != 0){
+            int temp = a%b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    public static void process(){
         arr[1] = 3;
+
+
         for(int i = 2 ; i <= 1000 ; i++){
-            int cnt = 0;
+            int line = 0;
             for(int j = 1 ; j < i ; j++){
                 if(gcd(i,j) == 1){
-                    cnt+=2;
+                    line+=2;
                 }
             }
-            arr[i] = arr[i-1]+cnt;
-        }
 
-        for(int tc = 0 ; tc < T ; tc++){
-            N = Integer.parseInt(br.readLine());
-
-            process();
+            arr[i] = arr[i-1]+line;
         }
     }
 
-    //실행
-    public static void process() {
-        sb.append(arr[N]).append("\n");
-    }
-
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         input();
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
