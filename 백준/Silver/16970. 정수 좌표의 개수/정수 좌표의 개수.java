f4da6034b7def9e8bb2398cf7f28c90d1ad51ba3
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-03-29 22:38:45 +0900

    [Silver I] Title: 정수 좌표의 개수, Time: 164 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/16970. 정수 좌표의 개수/정수 좌표의 개수.java
+++ 백준/Silver/16970. 정수 좌표의 개수/정수 좌표의 개수.java
@@ -3,54 +3,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb;
     private static BufferedReader br;
+    private static StringBuilder sb;
     private static StringTokenizer st;
-    private static int N,M,K;
-    private static int answer = 0;
+    private static int N,M,K,answer;
 
-    //입력
+
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-
     }
-    public static int gcd(int x, int y){
-        if(x == 0 || y == 0){
-            return x + y;
+    public static int gcd(int a, int b){
+        if(a == 0 || b == 0){
+            return a+b;
         }
 
-        while(x % y != 0){
-            int temp = x % y;
-            x = y;
-            y = temp;
+        int temp = 0;
+        while (a % b != 0){
+            temp = a%b;
+            a = b;
+            b = temp;
         }
-        return y;
+
+        return b;
     }
 
-    //실행
     public static void process() {
         for(int x1 = 0 ; x1 <= N ; x1++){
             for(int y1 = 0 ; y1 <= M ; y1++){
                 for(int x2 = 0 ; x2 <= N ; x2++){
                     for(int y2 = 0 ; y2 <= M ; y2++){
-                        if(gcd(Math.abs(x2-x1), Math.abs(y2-y1)) + 1 == K){
+                        if(gcd(x2-x1, y2-y1)+1 == K){
                             answer++;
                         }
                     }
                 }
             }
         }
-        System.out.println(answer/2);
+
+        System.out.println(answer);
     }
 
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         input();
         process();
     }
