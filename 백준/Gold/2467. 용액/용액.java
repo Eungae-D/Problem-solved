45fe5ebd0cd5854f82a3d234d0b14e265693f8e6
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-04-08 20:59:52 +0900

    [Gold V] Title: 용액, Time: 324 ms, Memory: 28728 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -3,59 +3,58 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb;
     private static BufferedReader br;
+    private static StringBuilder sb;
     private static StringTokenizer st;
     private static int N;
     private static int[] arr;
     private static long min = Long.MAX_VALUE;
     private static int left,right;
 
-    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
-
         arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N; i++){
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
     }
 
-    //실행
-    public static void process() {
+    public static void process(){
         int s = 0;
         int e = N-1;
 
         while(s<e){
             long sum = arr[s]+arr[e];
 
-            if(Math.abs(sum)<min){
+            
+            if(Math.abs(sum) < min){
                 min = Math.abs(sum);
                 left = s;
                 right = e;
             }
-
-            if(sum<0){
-                s++;
-            }else if(sum>0){
+            if(sum > 0){
                 e--;
+            }else if(sum < 0){
+                s++;
             }else{
                 sb.append(arr[s]+" "+arr[e]);
                 return;
             }
         }
         sb.append(arr[left]+" "+arr[right]);
+        
+        //어이없네 sb.toString()이 여기있으면 틀려?
     }
 
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         input();
         process();
+        //어이없네 sb.toString()이 여기있으면 맞고
         System.out.println(sb.toString());
     }
 }
