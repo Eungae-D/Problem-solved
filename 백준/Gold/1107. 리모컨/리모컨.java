f6cafd0a9f4da6786b79c6cf6f42c1b67decf5c0
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-07-22 00:15:48 +0900

    [Gold IV] Title: 리모컨, Time: 132 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -7,50 +7,55 @@ public class Main {
     private static BufferedReader br;
     private static StringTokenizer st;
     private static int N,M;
-    private static boolean[]number = new boolean[10];
+    private static String n;
+    private static boolean[] number = new boolean[10];
     private static long answer;
 
-
+    //입력
     public static void input() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
+        n = br.readLine();
+        N = Integer.parseInt(n);
         M = Integer.parseInt(br.readLine());
+
 
         if(M > 0){
             st = new StringTokenizer(br.readLine());
-            for(int i = 0 ; i < M; i++){
+            for(int i = 0 ; i < M ; i++){
                 number[Integer.parseInt(st.nextToken())] = true;
             }
         }
     }
-
     public static void recur(int cur, int touch){
-       if(cur > 0){
-           int cnt = Math.abs(N-touch) + cur;
-           answer = Math.min(answer,cnt);
-       }
+        if( cur > 0 ){
+            int cnt = Math.abs(N-touch) + cur;
+            answer = Math.min(answer, cnt);
+        }
 
-        if(cur >= 6){
+        if(cur == n.length()+1){
             return;
         }
 
-       for(int i = 0 ; i < 10 ;  i++){
-           if(!number[i]){
-               recur(cur+1,touch*10+i);
-           }
-       }
+        if(cur==6){
+            return;
+        }
 
+        for(int i = 0 ; i < 10 ; i++){
+            if(!number[i]){
+                recur(cur+1, touch*10+i);
+            }
+        }
     }
 
+    //실행
     public static void process() {
-
-
         answer = Math.abs(N-100);
-
         recur(0,0);
         System.out.println(answer);
     }
+
 
     public static void main(String[] args) throws Exception {
         input();
