e32dac0daed8740ea546bc5d92a891b486be06db
GyuHo Choi <135101171+Eungae-D@users.noreply.github.com>
2025-11-28 22:01:25 +0900

    [Bronze I] Title: Lifeguards (Bronze), Time: 96 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/15593. Lifeguards （Bronze）/Lifeguards （Bronze）.java
+++ 백준/Bronze/15593. Lifeguards （Bronze）/Lifeguards （Bronze）.java
@@ -1,36 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static StringBuilder sb;
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static int N;
+    private static StringBuilder sb;
+    private static int n;
     private static int[][]arr;
     private static int[]time = new int[1010];
-    private static int totalCount = 0;
     private static int min = Integer.MAX_VALUE;
+    private static int totalCount = 0;
 
-    //입력
-    public static void input() throws Exception {
+    public static void input() throws Exception{
         br = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
 
-        arr = new int[N][2];
-        for(int i = 0 ; i < N ; i++){
+        arr = new int[n][2];
+        for(int i = 0 ; i < n ; i++){
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
     }
 
-    //실행
-    public static void process() {
-        for(int i = 0 ; i < N ; i++){
-            for(int j = arr[i][0] ; j < arr[i][1] ; j++){
+    public static void process(){
+        for(int i = 0; i < n; i++){
+            for(int j = arr[i][0] ; j <arr[i][1]; j++){
                 if(time[j] == 0){
                     totalCount++;
                 }
@@ -38,21 +37,19 @@ public class Main {
             }
         }
 
-        for(int i = 0 ; i < N ; i++){
+        for(int i = 0; i < n; i++){
             int count = 0;
-            for(int j = arr[i][0] ; j < arr[i][1] ; j++){
+            for(int j = arr[i][0] ; j <arr[i][1]; j++){
                 if(time[j] == 1){
                     count++;
                 }
             }
             min = Math.min(min,count);
         }
-
         System.out.println(totalCount-min);
     }
 
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         input();
         process();
     }
